92241f24b385f4d0a782017d477cae30e3da573e
Roberval Walsh <roberval.walsh@gmail.com>
2015-01-13 10:02:17 +0100

    Propagation of updates

--- MssmHbbAnalysis/Ntuplizer/test/ntuplizer_cfg.py
+++ MssmHbbAnalysis/Ntuplizer/test/ntuplizer_cfg.py
@@ -8,7 +8,8 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 ## TFileService
 process.TFileService = cms.Service("TFileService",
-	fileName = cms.string('/afs/desy.de/user/w/walsh/xxl-af-cms/cms/data/trigger_studies_718_old.root')
+#	fileName = cms.string('/afs/desy.de/user/w/walsh/xxl-af-cms/cms/data/trigger_studies_718_old.root')
+	fileName = cms.string('test.root')
 )
 
 # need some understanding before using; number of jets do not macth the sum.
@@ -20,24 +21,22 @@ process.TriggerStudies = cms.EDAnalyzer("Ntuplizer",
     MonteCarlo = cms.bool(True),
     L1ExtraJets = cms.VInputTag(cms.InputTag("hltL1extraParticles","Merged","Demo"),cms.InputTag("hltL1extraParticles","Central","TEST"),cms.InputTag("hltL1extraParticles","Tau","TEST"),cms.InputTag("hltL1extraParticles","Forward","TEST")),
     CaloJets = cms.VInputTag(cms.InputTag("hltAK4CaloJets"),cms.InputTag("hltAK4CaloJetsCorrected"),cms.InputTag("hltAK4CaloJetsCorrectedIDPassed"),cms.InputTag("hltAK4CaloJetsIDPassed")),
-    PFJets = cms.VInputTag(cms.InputTag("hltAK4PFJets"),cms.InputTag("hltAK4PFJetsCorrected")),
+#    PFJets = cms.VInputTag(cms.InputTag("hltAK4PFJets"),cms.InputTag("hltAK4PFJetsCorrected")),
 #    GenJets = cms.VInputTag(cms.InputTag("ak5GenJets")),
 #    PileupInfo = cms.VInputTag(cms.InputTag("addPileupInfo")),
 )
 
-#process.p = cms.Path(process.hltL1extraParticles*process.TriggerStudies)
-process.p = cms.Path(process.TriggerStudies)
+process.p = cms.Path(process.hltL1extraParticles*process.TriggerStudies)
+#process.p = cms.Path(process.TriggerStudies)
 
 readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring() 
 process.source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
 readFiles.extend( [
-       '/store/user/rwalsh/QCD_Pt-15to3000_Tune4C_Flat_13TeV_pythia8/Release718-TriggerStudies_Dev712-OutTriggerOnly-v1/141020_111529/0000/trigger_studies_skim_1.root',
+       'file:/afs/desy.de/user/w/walsh/xxl-af-cms/cms/data/trigger_studies_730_mssmhbb_v7_skim.root'
+#       '/store/user/rwalsh/QCD_Pt-15to3000_Tune4C_Flat_13TeV_pythia8/Release718-TriggerStudies_Dev712-OutTriggerOnly-v1/141020_111529/0000/trigger_studies_skim_1.root',
 #       '/store/mc/Spring14dr/QCD_Pt-15to3000_Tune4C_Flat_13TeV_pythia8/AODSIM/Flat20to50_POSTLS170_V5-v1/00000/02007198-D1DD-E311-A7A7-E0CB4E29C4D3.root',
                 ] );
-readFiles.extend( [
-                ] );
-
 
 secFiles.extend( [
                ] )
