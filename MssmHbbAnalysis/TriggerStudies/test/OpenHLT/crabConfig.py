a50b487f1fc9137824a8880e3a438efb0fb8be15
Roberval Walsh <roberval.walsh@gmail.com>
2015-04-17 17:08:00 +0200

    some minor updates

--- MssmHbbAnalysis/TriggerStudies/test/OpenHLT/crabConfig.py
+++ MssmHbbAnalysis/TriggerStudies/test/OpenHLT/crabConfig.py
@@ -2,25 +2,30 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 config.section_('General')
 config.General.transferOutputs = True
-DATASET  = 'SUSYBBHToBB_M-300_13TeV-pythia6-tauola'
-DATANAME = 'OpenHLT-740-hlt_run2_mssmhbb_scratch_V23-Producers-v1'
+DATASET  = 'SUSYBBHToBB_M-300_13TeV'
+DATANAME = 'OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v2'
 config.General.requestName = DATASET+'_'+DATANAME
 
 config.section_('JobType')
-config.JobType.psetName = 'openhlt_go_prod.py'
+#config.JobType.psetName = 'openhlt_go_prod.py'
+config.JobType.psetName = 'openhlt_go_filt.py'
 config.JobType.pluginName = 'Analysis'
-config.JobType.inputFiles = ['hlt_run2_mssmhbb_scratch_V23_cfg.py', ' setup_740_GRun_V34_cff.py']
-config.JobType.outputFiles = ['Producers.root']
+#config.JobType.inputFiles = ['hlt_run2_mssmhbb_scratch_V47_cfg.py', 'setup_740_GRun_V36_cff.py']
+config.JobType.inputFiles = ['hlt_run2_mssmhbb_scratch_V47_OnlineBTag_cfg.py', 'setup_740_GRun_V36_cff.py']
+#config.JobType.outputFiles = ['Producers.root']
+config.JobType.outputFiles = ['Filters.root']
 
 config.section_('Data')
-config.Data.inputDataset = '/'+DATASET+'/yechen-SUSYBBHToBB_M-300_13TeV-pythia6-tauola-RAWSIM-ave_40_bx_25ns-d065fd20afeab0487c05d89037cc39c1/USER'
-config.Data.publishDBS = 'phys03'
-config.Data.publication = True
-config.Data.unitsPerJob = 200
-config.Data.totalUnits = 100000
+#config.Data.inputDataset = '/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/yechen-SUSYBBHToBB_M-300_13TeV-pythia6-tauola-RAWSIM-ave_40_bx_25ns-d065fd20afeab0487c05d89037cc39c1/USER'
+config.Data.inputDataset = '/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/rwalsh-OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Prod-v1-4a506ec4fefd912077c1330a00458e1d/USER'
 config.Data.inputDBS = 'phys03'
-config.Data.splitting = 'EventBased'
+config.Data.publication = True
+config.Data.publishDBS = 'phys03'
 config.Data.publishDataName = DATANAME
+#config.Data.unitsPerJob = 4
+config.Data.unitsPerJob = 1
+#config.Data.totalUnits = 250
+config.Data.splitting = 'FileBased'
 
 config.section_('User')
 config.User.voGroup = 'dcms'
--- MssmHbbAnalysis/TriggerStudies/test/OpenHLT/ntuplizer_cfg.py
+++ MssmHbbAnalysis/TriggerStudies/test/OpenHLT/ntuplizer_cfg.py
@@ -33,15 +33,12 @@ process.TriggerStudies = cms.EDAnalyzer("Ntuplizer",
     L1ExtraJets = cms.VInputTag(cms.InputTag("hltL1extraParticles","Merged","Demo"),cms.InputTag("hltL1extraParticles","Central","HLT3"),cms.InputTag("hltL1extraParticles","Tau","HLT3"),cms.InputTag("hltL1extraParticles","Forward","HLT3")),
     CaloJets = cms.VInputTag(cms.InputTag("hltAK4CaloJets"),cms.InputTag("hltAK4CaloJetsCorrected"),cms.InputTag("hltAK4CaloJetsCorrectedIDPassed"),cms.InputTag("hltAK4CaloJetsIDPassed")),
     PFJets = cms.VInputTag(cms.InputTag("hltAK4PFJets"),cms.InputTag("hltAK4PFJetsCorrected"),cms.InputTag("hltAK4PFJetsCorrectedLooseID")),
-    JetsTags = cms.VInputTag(cms.InputTag("hltL3CombinedSecondaryVertexBJetTags")),
+    JetsTags = cms.VInputTag(cms.InputTag("hltCombinedSecondaryVertexBJetTagsCalo","","HLT3PB"),cms.InputTag("hltCombinedSecondaryVertexBJetTagsPF","","HLT3PB")),
     GenJets = cms.VInputTag(cms.InputTag("ak5GenJets")),
     TriggerResults = cms.VInputTag(cms.InputTag("TriggerResults","","HLT3PB"),cms.InputTag("TriggerResults","","HLT3")),
     TriggerPaths = cms.vstring( "HLT_ZeroBias_v1",
-                                "HLT_L1DoubleJetC100_v1",
-                                "HLT_DoubleJet100Eta2p6_DoubleCSV0p5_v1",
-                                "HLT_DoubleJet100Eta2p6_DoubleCSV0p5_Changed_CSV0p8_v1",
-                                "HLT_DoubleJet100Eta2p6_DoubleCSV0p5_Changed_Eta1p7_v1",
-                                "HLT_DoubleJet100Eta2p6_DoubleCSV0p5_Changed_Eta1p7_and_CSV0p8_v1")
+                                "HLT_PFJet_BTagCSV_v1",
+                                "HLT_PFJet_BTagCSV_Changed_OnlineBTag_v1")
 #    PileupInfo = cms.VInputTag(cms.InputTag("addPileupInfo")),
 )
 
@@ -52,7 +49,7 @@ readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring() 
 process.source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
 readFiles.extend( [
-       'file:Filt.root'
+       'file:Filters.root'
 #       'file:/afs/desy.de/user/w/walsh/xxl-af-cms/cms/data/trigger_studies_730_mssmhbb_v7_skim.root'
 #       '/store/user/rwalsh/QCD_Pt-15to3000_Tune4C_Flat_13TeV_pythia8/Release718-TriggerStudies_Dev712-OutTriggerOnly-v1/141020_111529/0000/trigger_studies_skim_1.root',
 #       '/store/mc/Spring14dr/QCD_Pt-15to3000_Tune4C_Flat_13TeV_pythia8/AODSIM/Flat20to50_POSTLS170_V5-v1/00000/02007198-D1DD-E311-A7A7-E0CB4E29C4D3.root',
