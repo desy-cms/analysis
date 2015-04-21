f121c9a47ed65e61a0da57ad469608857798d35d
Roberval Walsh <roberval.walsh@gmail.com>
2015-04-21 18:23:06 +0200

    modifications

--- MssmHbbAnalysis/TriggerStudies/test/OpenHLT/ntuplizer_cfg.py
+++ MssmHbbAnalysis/TriggerStudies/test/OpenHLT/ntuplizer_cfg.py
@@ -20,7 +20,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 ## TFileService
 process.TFileService = cms.Service("TFileService",
 #	fileName = cms.string('/afs/desy.de/user/w/walsh/xxl-af-cms/cms/data/trigger_studies_718_old.root')
-	fileName = cms.string('tests.root')
+	fileName = cms.string('ntuple_v3.root')
 )
 
 # trigger filter
@@ -31,7 +31,7 @@ process.triggerResultsFilter = cms.EDFilter('TriggerResultsFilter',
    l1techIgnorePrescales = cms.bool(False), # read L1 technical bits from PSB#9, bypassing the prescales
    daqPartitions = cms.uint32(0x01), # used by the definition of the L1 mask
    throw = cms.bool(False), # throw exception on unknown trigger names
-   triggerConditions = cms.vstring( 'HLT_PFJet_BTagCSV_Changed_OnlineBTag_v1' )
+   triggerConditions = cms.vstring( 'HLT_PFJet_BTagCSV_v1' )
 )
 # need some understanding before using; number of jets do not macth the sum.
 process.hltL1extraParticles = cms.EDProducer("L1ExtraJetsMerger",
@@ -45,10 +45,13 @@ process.TriggerStudies = cms.EDAnalyzer("Ntuplizer",
     PFJets = cms.VInputTag(cms.InputTag("hltAK4PFJets"),cms.InputTag("hltAK4PFJetsCorrected"),cms.InputTag("hltAK4PFJetsCorrectedLooseID")),
     JetsTags = cms.VInputTag(cms.InputTag("hltCombinedSecondaryVertexBJetTagsCalo","","HLT3PB"),cms.InputTag("hltCombinedSecondaryVertexBJetTagsPF","","HLT3PB")),
     GenJets = cms.VInputTag(cms.InputTag("ak5GenJets")),
+    PrimaryVertices = cms.VInputTag(cms.InputTag("hltVerticesPF"),cms.InputTag("hltVerticesPFSelector"),cms.InputTag("hltVerticesPFFilter"),cms.InputTag("hltVerticesL3")),
     TriggerResults = cms.VInputTag(cms.InputTag("TriggerResults","","HLT3PB"),cms.InputTag("TriggerResults","","HLT3")),
     TriggerPaths = cms.vstring( "HLT_ZeroBias_v1",
                                 "HLT_PFJet_BTagCSV_v1",
-                                "HLT_PFJet_BTagCSV_Changed_OnlineBTag_v1")
+                                "HLT_PFJet_BTagCSV_Changed_CSVL3_0p41_v1",
+                                "HLT_PFJet_BTagCSV_Changed_CSVL3_0p54_v1",
+                                "HLT_PFJet_BTagCSV_Changed_CSVL3_0p63_v1",)
 #    PileupInfo = cms.VInputTag(cms.InputTag("addPileupInfo")),
 )
 
@@ -68,3 +71,72 @@ readFiles.extend( [
 secFiles.extend( [
                ] )
 
+#readFiles.extend( [
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_1.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_10.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_11.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_12.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_13.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_14.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_15.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_16.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_17.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_18.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_19.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_2.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_20.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_21.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_22.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_23.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_24.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_25.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_26.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_27.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_28.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_29.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_3.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_30.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_31.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_32.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_33.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_34.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_35.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_36.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_37.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_38.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_39.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_4.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_40.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_41.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_42.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_43.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_44.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_45.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_46.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_47.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_48.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_49.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_5.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_50.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_51.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_52.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_53.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_54.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_55.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_56.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_57.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_58.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_59.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_6.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_60.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_61.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_62.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_63.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_7.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_8.root',
+#       '/store/user/rwalsh/SUSYBBHToBB_M-300_13TeV-pythia6-tauola/OpenHLT-740-hlt_run2_mssmhbb_scratch_V47-Filt-v3/150417_161120/0000/Filters_9.root' ] );
+#
+#
+#secFiles.extend( [
+#               ] )
+#
