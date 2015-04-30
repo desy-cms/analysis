eecb6016811cf3cf2816e962ca3400e4ab7014a4
Roberval Walsh <roberval.walsh@gmail.com>
2015-04-30 10:03:17 +0200

    Added ak4GenJets sequence; added cross section read from a tree

--- MssmHbbAnalysis/MonteCarloStudies/test/ntuplizer_gen.py
+++ MssmHbbAnalysis/MonteCarloStudies/test/ntuplizer_gen.py
@@ -17,29 +17,82 @@ for pset in process.GlobalTag.toGet.value():
 process.GlobalTag.RefreshEachRun = cms.untracked.bool( False )
 process.GlobalTag.ReconnectEachRun = cms.untracked.bool( False )
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(5000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
 
 ## TFileService
 process.TFileService = cms.Service("TFileService",
-	fileName = cms.string('ntuple_susybbHtobb-M300_genjets.root')
+#	fileName = cms.string('/nfs/dust/cms/user/walsh/analysis/higgs/generated/signal/ntuple_susybbHtobb-M300_genjets.root')
+	fileName = cms.string('/nfs/dust/cms/user/walsh/analysis/higgs/generated/background/ntuple_qcd50to80_genjets.root')
 )
 
+# GenJets (rerun when not available)
+process.load('RecoJets/Configuration/GenJetParticles_cff')
+process.load('RecoJets/JetProducers/ak4GenJets_cfi')
+
+# Ntuplizer
 process.MonteCarloStudies = cms.EDAnalyzer("Ntuplizer",
     MonteCarlo      = cms.bool(True),
+    CrossSection    = cms.double(2.211e07),
     GenJets         = cms.VInputTag(cms.InputTag("ak4GenJets")),
 )
 
-process.p = cms.Path(process.MonteCarloStudies)
+#process.p = cms.Path(process.MonteCarloStudies)
+process.p = cms.Path(process.genJetParticles+process.ak4GenJets+process.MonteCarloStudies)
 
 readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring() 
 process.source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
 readFiles.extend( [
-       '/store/mc/RunIIWinter15GS/SUSYGluGluToBBHToBB_M-300_TuneCUETP8M1_13TeV-pythia8/GEN-SIM/MCRUN2_71_V1-v1/70000/1E5BB9A7-19E9-E411-98AA-0023AEFDEC3C.root',
-       '/store/mc/RunIIWinter15GS/SUSYGluGluToBBHToBB_M-300_TuneCUETP8M1_13TeV-pythia8/GEN-SIM/MCRUN2_71_V1-v1/70000/384C3E92-4FE8-E411-B572-0023AEFDE9C8.root',
-       '/store/mc/RunIIWinter15GS/SUSYGluGluToBBHToBB_M-300_TuneCUETP8M1_13TeV-pythia8/GEN-SIM/MCRUN2_71_V1-v1/70000/C20B0795-2BE8-E411-975F-008CFA1CB55C.root',
-       '/store/mc/RunIIWinter15GS/SUSYGluGluToBBHToBB_M-300_TuneCUETP8M1_13TeV-pythia8/GEN-SIM/MCRUN2_71_V1-v1/70000/E492AEA0-3DE8-E411-AE51-008CFA06477C.root',
-] );
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_1.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_10.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_11.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_12.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_13.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_14.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_15.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_16.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_17.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_18.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_19.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_2.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_20.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_21.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_22.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_23.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_24.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_25.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_26.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_27.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_28.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_29.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_3.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_30.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_31.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_32.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_33.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_34.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_35.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_36.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_37.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_38.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_39.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_4.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_40.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_41.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_42.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_43.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_44.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_45.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_46.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_47.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_48.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_49.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_5.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_50.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_6.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_7.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_8.root',
+       '/store/user/rwalsh/QCD_Pt-50to80_Tune4C_13TeV_pythia8/OpenHLT-740p1_MCRUN2_72_V3-hlt_run2_mssmhbb_scratch_V47-Producers-v1/150421_171406/0000/Producers_9.root' ] );
 
 
 secFiles.extend( [
