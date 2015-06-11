1a0537aacd0937d1756e54cc169f986a86586e3b
Roberval Walsh <roberval.walsh@gmail.com>
2015-06-11 19:19:22 +0200

    Analysis codes (under dev)

---
+++ MssmHbbAnalysis/Analysis/bin/NtupleVariables.h
@@ -0,0 +1,56 @@
+#ifndef MssmHbbAnalysis_Analysis_bin_NtupleVariables_h
+#define MssmHbbAnalysis_Analysis_bin_NtupleVariables_h
+
+#include <memory>
+#include <map>
+#include <string>
+
+typedef std::map<std::string, bool>::iterator it_trg;
+typedef std::map<std::string, int>::iterator it_cnt;
+
+// Variables declaration
+
+static const int maxCands_ = 100;
+
+// Metadata
+double xsection_;
+unsigned int nGenTotal_;
+unsigned int nGenFilt_;
+
+// EventInfo
+int run_;
+int lumi_;
+int event_;
+int bx_;
+int orbit_;
+
+
+// jets
+int   jetN_   ;
+float jetPt_  [maxCands_];
+float jetE_   [maxCands_];
+float jetEt_  [maxCands_];
+float jetEta_ [maxCands_];
+float jetPhi_ [maxCands_];
+float jetBtag_[maxCands_];
+int   jetFlav_[maxCands_];
+
+// muons
+int   muN_   ;
+float muPt_ [maxCands_];
+float muE_  [maxCands_];
+float muEt_ [maxCands_];
+float muEta_[maxCands_];
+float muPhi_[maxCands_];
+float muQ_  [maxCands_];
+
+
+// trigger accepts
+// bool * trgAccepts_;
+// std::unique_ptr<bool[]> trgAccepts_;
+std::map<std::string, bool> trgAccepts_;
+
+#endif
+
+
+
