99da2c124c3d8a5f2859d821e82e0480b7d490ca
Roberval Walsh <roberval.walsh@gmail.com>
2015-06-15 13:21:59 +0200

    Improvements: command line options; etc

--- MssmHbbAnalysis/Analysis/bin/TagProbeOnlineBtag.cc
+++ MssmHbbAnalysis/Analysis/bin/TagProbeOnlineBtag.cc
@@ -41,12 +41,14 @@ int main(int argc, char * argv[])
    TH1::SetDefaultSumw2();
    TH2::SetDefaultSumw2();
    
-   lumi = 1.; // in fb-1
+   lumi = 20.; // in fb-1
    
    std::map<std::string, TH1F*> h1;
    
    std::string triggerPath = "HLT_BTagMu_DiJet20_Mu5";
-   std::string dataset = "QCD_Pt_30to50_Mu_Pt5";
+   std::string dataset = "30to50";
+   std::string type = "data";
+   std::string ptbin = "nobin";
    
    if ( argc == 2 )
    {
@@ -57,9 +59,22 @@ int main(int argc, char * argv[])
       dataset = argv[1];
       triggerPath = argv[2];
    }
+   else if ( argc == 4 ) 
+   {
+      dataset = argv[1];
+      triggerPath = argv[2];
+      type = argv[3];
+   }
+   else if ( argc == 5 ) 
+   {
+      dataset = argv[1];
+      triggerPath = argv[2];
+      type = argv[3];
+      ptbin = argv[4];
+   }
    
-   h1["h_jetTagPt"]  = new TH1F("h_jetTagPt", "", 500, 0., 500.);
-   h1["h_jetProbePt"]= new TH1F("h_jetProbePt", "", 500, 0., 500.);
+   h1["h_jetTagPt"]  = new TH1F("h_jetTagPt", "", 100, 0., 500.);
+   h1["h_jetProbePt"]= new TH1F("h_jetProbePt", "", 100, 0., 500.);
    h1["h_jetTPdPhi"] = new TH1F("h_dPhiJetTP", "", 64, 0., 3.2);
    
    h1["h_jetTagFlavour"]  = new TH1F("h_jetTagFlavour", "", 35, -10, 25.);
@@ -68,7 +83,7 @@ int main(int argc, char * argv[])
    h1["h_jetTagBtag"]  = new TH1F("h_jetTagBtag", "", 100, 0., 1.);
    h1["h_jetProbeBtag"]= new TH1F("h_jetProbeBtag", "", 100, 0., 1.);
    
-   h1["h_SelChain"] = new TH1F("h_SelChain", "", 7, 0., 7.);
+   h1["h_SelChain"] = new TH1F("h_SelChain", "", 8, 0., 8.);
    h1["h_SelChain"] -> GetXaxis() -> SetBinLabel(1,"no cut");
    h1["h_SelChain"] -> GetXaxis() -> SetBinLabel(2,"btagmu trigger");
    h1["h_SelChain"] -> GetXaxis() -> SetBinLabel(3,"online btag (emul)");
@@ -76,6 +91,7 @@ int main(int argc, char * argv[])
    h1["h_SelChain"] -> GetXaxis() -> SetBinLabel(5,"muon in jet (tag jet)");
    h1["h_SelChain"] -> GetXaxis() -> SetBinLabel(6,"offline 2 btags");
    h1["h_SelChain"] -> GetXaxis() -> SetBinLabel(7,"delta phi(j1,j2)");
+   h1["h_SelChain"] -> GetXaxis() -> SetBinLabel(8,"prescale");
    
    
    // TTree chains and friendship?
@@ -175,6 +191,49 @@ int main(int argc, char * argv[])
       }
    }
    
+   std::map<std::string, float> ps; 
+   ps["HLT_BTagMu_DiJet20_Mu5"]  = 400;
+   ps["HLT_BTagMu_DiJet40_Mu5"]  = 200;
+   ps["HLT_BTagMu_DiJet70_Mu5"]  = 40;
+   ps["HLT_BTagMu_DiJet110_Mu5"] = 10;
+   ps["HLT_BTagMu_Jet300_Mu5"]   = 1;
+   if ( type == "mc" )
+   {
+      ps["HLT_BTagMu_DiJet20_Mu5"]  = 1;
+      ps["HLT_BTagMu_DiJet40_Mu5"]  = 1;
+      ps["HLT_BTagMu_DiJet70_Mu5"]  = 1;
+      ps["HLT_BTagMu_DiJet110_Mu5"] = 1;
+      ps["HLT_BTagMu_Jet300_Mu5"]   = 1;
+   }
+
+   std::map<std::string, float> jetptmin; 
+   jetptmin["HLT_BTagMu_DiJet20_Mu5"]  = 30;
+   jetptmin["HLT_BTagMu_DiJet40_Mu5"]  = 30;
+   jetptmin["HLT_BTagMu_DiJet70_Mu5"]  = 30;
+   jetptmin["HLT_BTagMu_DiJet110_Mu5"] = 30;
+   jetptmin["HLT_BTagMu_Jet300_Mu5"]   = 30;
+
+   std::map<std::string, float> jetptmax; 
+   jetptmax["HLT_BTagMu_DiJet20_Mu5"]  = 999999.; 
+   jetptmax["HLT_BTagMu_DiJet40_Mu5"]  = 999999.; 
+   jetptmax["HLT_BTagMu_DiJet70_Mu5"]  = 999999.; 
+   jetptmax["HLT_BTagMu_DiJet110_Mu5"] = 999999.; 
+   jetptmax["HLT_BTagMu_Jet300_Mu5"]   = 999999.; 
+
+   if ( ptbin != "nobin" )
+   {
+      jetptmin["HLT_BTagMu_DiJet20_Mu5"]  = 30;    
+      jetptmin["HLT_BTagMu_DiJet40_Mu5"]  = 50;    
+      jetptmin["HLT_BTagMu_DiJet70_Mu5"]  = 70;    
+      jetptmin["HLT_BTagMu_DiJet110_Mu5"] = 120;   
+      jetptmin["HLT_BTagMu_Jet300_Mu5"]   = 300;   
+      jetptmax["HLT_BTagMu_DiJet20_Mu5"]  = 50.;   
+      jetptmax["HLT_BTagMu_DiJet40_Mu5"]  = 70.;   
+      jetptmax["HLT_BTagMu_DiJet70_Mu5"]  = 120.;  
+      jetptmax["HLT_BTagMu_DiJet110_Mu5"] = 300.;  
+      jetptmax["HLT_BTagMu_Jet300_Mu5"]   = 999999.;
+   }
+   
    std::cout << triggerPath_v << std::endl;
 
    // Number of loop events
@@ -183,6 +242,8 @@ int main(int argc, char * argv[])
    int eventCounter = 0;
    int btagEmulationCounter = 0;
    int triggerCounter = 0;
+   float weight;
+   
    for ( int i = 0 ; i < nEvents ; ++i )
    {
       if ( i%10000 == 0 ) std::cout << "Processed  " << i << "  events" << "\xd";
@@ -190,13 +251,15 @@ int main(int argc, char * argv[])
       
       chainEvent->GetEntry(i);
       
-      h1["h_SelChain"] -> Fill("no cut",1);
+      weight = 1.;
+      
+      h1["h_SelChain"] -> Fill("no cut",weight);
       
       // trigger selection
       if ( ! trgAccepts_[triggerPath_v] ) continue;
       ++triggerCounter;
       
-      h1["h_SelChain"] -> Fill("btagmu trigger",1);
+      h1["h_SelChain"] -> Fill("btagmu trigger",weight);
       
       // rough emulation  of the btag leg using offline variables (for trigger rate/xsection evaluation)
       // it should not have any impact in the final selection statistics
@@ -214,7 +277,8 @@ int main(int argc, char * argv[])
       if ( ! hasBJet ) continue;
       
       ++btagEmulationCounter;
-      h1["h_SelChain"] -> Fill("online btag (emul)",1);
+      weight *= 0.5;
+      h1["h_SelChain"] -> Fill("online btag (emul)",weight);
       
       // At least 2 jets, and at least one muon
       if ( jetN_ < 2 ) continue;
@@ -226,10 +290,10 @@ int main(int argc, char * argv[])
       jet[1].SetPtEtaPhiE(jetPt_[1], jetEta_[1], jetPhi_[1], jetE_[1]);
       
       // Jet kinematics selection
-      if ( jet[0].Pt() < 30. || fabs(jet[0].Eta()) > 2.4  ) continue;
-      if ( jet[1].Pt() < 30. || fabs(jet[1].Eta()) > 2.4  ) continue;
+      if ( jet[0].Pt() < jetptmin[triggerPath] || jet[0].Pt() > jetptmax[triggerPath] || fabs(jet[0].Eta()) > 2.4 ) continue;
+      if ( jet[1].Pt() < jetptmin[triggerPath] || jet[1].Pt() > jetptmax[triggerPath] || fabs(jet[1].Eta()) > 2.4 ) continue;
       
-      h1["h_SelChain"] -> Fill("dijet selection",1);
+      h1["h_SelChain"] -> Fill("dijet selection",weight);
 
       // Tag and probe      
       TLorentzVector tagJet;
@@ -273,7 +337,7 @@ int main(int argc, char * argv[])
       }
       
       if ( ! muInJet ) continue;
-      h1["h_SelChain"] -> Fill("muon in jet (tag jet)",1);
+      h1["h_SelChain"] -> Fill("muon in jet (tag jet)",weight);
 
       // offline btagging
       // for the probe jet it must be the nominal btag wp
@@ -281,13 +345,19 @@ int main(int argc, char * argv[])
       // for the tag jet it can be tigher
       if( tagJetBtag < 0.95 ) continue;
       
-      h1["h_SelChain"] -> Fill("offline 2 btags",1);
+      h1["h_SelChain"] -> Fill("offline 2 btags",weight);
       
       // delta_phi cut
       float jetTPdPhi = fabs(tagJet.DeltaPhi(probeJet));
       if ( jetTPdPhi < 2.5 ) continue;
       
-      h1["h_SelChain"] -> Fill("delta phi(j1,j2)",1);
+      h1["h_SelChain"] -> Fill("delta phi(j1,j2)",weight);
+      
+      
+      weight /= ps[triggerPath];
+      h1["h_SelChain"] -> Fill("prescale",weight);
+      
+      
       
       // Histograms
       h1["h_jetTagPt"] -> Fill(tagJet.Pt());      
@@ -300,27 +370,6 @@ int main(int argc, char * argv[])
       h1["h_jetProbeBtag"] -> Fill(probeJetBtag);
       
             
-//       if ( i == 0 ) 
-//       {
-//          std::cout << "Triggers" << std::endl;
-//          for ( auto& it : trgAccepts_ )
-//          {
-//             std::cout << it.first << "   " << it.second << std::endl;
-//          }
-//          std::cout << "Jets" << std::endl;
-//          for ( int i = 0; i < jetN_ ; ++i )
-//          {
-//             std::cout << "    " << jetPt_[i] << ",  " <<  jetEta_[i] << ",  " <<jetPhi_[i] << " ,  " << jetFlav_[i] <<std::endl;
-//          }
-//          std::cout << "Muons" << std::endl;
-//          for ( int i = 0; i < muN_ ; ++i )
-//          {
-//             std::cout << "    " << muPt_[i] << ",  " <<  muEta_[i] << ",  " << muPhi_[i] <<std::endl;
-//          }
-//       }
-         
-      
-      
    } // END LOOP EVENTS
    std::cout << "Processed  " << eventCounter << "  events" << "\n";
     
@@ -338,123 +387,28 @@ int main(int argc, char * argv[])
    std::cout << "Number of triggered events = " << triggerCounter << " events" << std::endl;
    std::cout << "Number of emulated btag events = " << btagEmulationCounter << " events" << std::endl;
     
-   
+   if ( type == "mc" )
+      scaleLumi = 1.;
       
-//       // The trigger objects after online selection, ie all objects that pass the selection
-//       std::vector<TLorentzVector> jets;
-//       
-//       // leading jets and preselection
-//       jets.clear();
-//       bool ok = true;
-//       if ( jetN_[0] < 3 ) continue; // at least 3 jets in the event
-//       for ( int j = 0 ; j < jetN_[0] ; ++j )
-//       {
-//          if ( j < 3 && fabs(jetEta_[0][j]) > 2.4 ) ok = false;  // 3 leading jets within the tracker acceptance
-//          if ( jetPt_[0][2] < 30 ) ok = false;  // 3rd leading jet soft cut
-//          TLorentzVector jet;
-//          jet.SetPtEtaPhiE(jetPt_[0][j], jetEta_[0][j], jetPhi_[0][j], jetE_[0][j]); 
-//          jets.push_back(jet);
-//       }
-//       // must have at least 3 jets
-//       if ( jets.size() < 3 ) continue;
-//       
-//       // deltaR between 3 leading jets must be > 1
-//       if ( jets[0].DeltaR(jets[1]) < 1. || jets[1].DeltaR(jets[2]) < 1. || jets[2].DeltaR(jets[0]) < 1. ) ok = false;
-//       
-//       // preselection
-//       if ( ! ok ) continue;
-//       
-//       TLorentzVector dijet = jets[0] + jets[1];
-//       
-//       float dEta01 = fabs(jets[0].Eta() - jets[1].Eta());
-//       float dEta12 = fabs(jets[1].Eta() - jets[2].Eta());
-//       float dEta02 = fabs(jets[0].Eta() - jets[2].Eta());
-//       
-//       float dR01 = jets[0].DeltaR(jets[1]);
-//       float dR12 = jets[1].DeltaR(jets[2]);
-//       float dR02 = jets[0].DeltaR(jets[2]);
-//       
-//       float m12 = dijet.M();
-//       
-//       if ( !(      jets[0].Pt()   > ptcut [0]) )   continue;
-//       if ( !( fabs(jets[0].Eta()) < etacut[0] ) )  continue;
-//       if ( !(      jets[1].Pt()   > ptcut [1]) )   continue;
-//       if ( !( fabs(jets[1].Eta()) < etacut[1] ) )  continue;
-//       if ( !(      jets[2].Pt()   > ptcut [2] ) )  continue;
-//       if ( !( fabs(jets[2].Eta()) < etacut[2] ) )  continue;
-//       if ( detacut > 0 )
-//          if ( dEta01 > detacut ) continue;
-//       if ( mw > 0 && dmw > 0 )
-//          if ( m12 < (mw-dmw) || m12 > (mw+dmw) ) continue;
-//       
-//       for ( size_t j = 0 ; j < jets.size() ; ++j )
-//       {
-//          if ( j > 3 ) break; // only up to 4 leading jets
-//          h_PtJet[j]  -> Fill(jets[j].Pt());
-//          h_EtaJet[j] -> Fill(jets[j].Eta());
-//       }
-//       h_Jet0Jet1_Pt  -> Fill(jets[0].Pt(),jets[1].Pt());
-//       h_Jet0Jet1_Eta -> Fill(fabs(jets[0].Eta()),fabs(jets[1].Eta()));
-//       h_MassJet01 -> Fill(m12);
-//       h_dEta01 -> Fill(dEta01);
-//       h_dEta12 -> Fill(dEta12);
-//       h_dEta02 -> Fill(dEta02);
-//       h_dR01 -> Fill(dR01);
-//       h_dR12 -> Fill(dR12);
-//       h_dR02 -> Fill(dR02);
-//       h_selection -> Fill(1.5);
-//    }
-//    /// end of loop event
-//    
-//    
-//    
-//    std::ostringstream tmp;
-//    tmp << "genjets_jpt" << int(ptcut[0]) << "_jpt" << int(ptcut[1]) << "_jpt" << int(ptcut[2]);
-//    tmp << "_jeta" << etacut[0] << "_jeta" << etacut[1] << "_jeta" << etacut[2];
-//    if ( detacut > 0 ) tmp << "_deta" << detacut;
-//    if ( mw > 0 && dmw > 0  ) tmp << "_m12w" << mw << "pm" << dmw;
-//    outFilename = TString(tmp.str());
-//    outFilename.ReplaceAll(".","p");
-//    
-//   outFilename = "genjets_"+subfilename+".jpt100_jpt100_jpt30_jeta2p4_jeta2p4_jeta2p4";
-   outFilename = dataset + "-" + triggerPath;
+   outFilename = type + "/";
+   if ( ptbin == "nobin" )
+      outFilename += "ptMin/";
+   else
+      outFilename += "ptMinMax/";
+   
+   outFilename += "QCD_Pt_"+ dataset + "_Mu_Pt5" + "-" + triggerPath;
    TFile * outFile = new TFile(outFilename+".root","RECREATE");
    
    for ( auto& ih1 : h1 )
    {
-      ih1.second -> Scale(scaleLumi);
+      if ( ih1.first == "h_SelChain" )
+         ih1.second -> Scale(scaleLumi);
+      else
+         ih1.second -> Scale(scaleLumi*0.5/ps[triggerPath]);
       ih1.second -> Write();
    }
    
    outFile -> Close();
-//    // scale to luminosity; write to file
-//    for ( size_t j = 0 ; j < 4 ; ++j )
-//    {
-//       h_PtJet[j]  -> Scale(scale);
-//       h_EtaJet[j] -> Scale(scale);
-//       h_PtJet[j]  -> Write();
-//       h_EtaJet[j] -> Write();
-//    }
-//    h_Jet0Jet1_Pt  -> Scale(scale);
-//    h_Jet0Jet1_Eta -> Scale(scale);
-//    h_MassJet01    -> Scale(scale);
-//    h_selection    -> Scale(scale);
-//    
-//    
-//    h_Jet0Jet1_Pt  -> Write();
-//    h_Jet0Jet1_Eta -> Write();
-//    h_MassJet01    -> Write();
-//    h_dEta01    -> Write();
-//    h_dEta12    -> Write();
-//    h_dEta02    -> Write();
-//    h_dR01    -> Write();
-//    h_dR12    -> Write();
-//    h_dR02    -> Write();
-//    
-//    h_selection    -> Write();
-//    
-//    outFile -> Close();
-    
 //    
 }
 
