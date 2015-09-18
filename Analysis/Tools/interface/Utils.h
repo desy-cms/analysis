326573346a4133fc19a04d5006b3215f5c019507
Roberval Walsh <roberval.walsh@gmail.com>
2015-09-18 17:50:45 +0200

    Some util stuff, struct etc

---
+++ Analysis/Tools/interface/Utils.h
@@ -0,0 +1,17 @@
+#ifndef Analysis_Tools_Utils_h
+#define Analysis_Tools_Utils_h 1
+
+namespace analysis {
+   namespace tools {
+
+      struct FilterResults
+      {
+         int   total;
+         int   filtered;
+         float efficiency;
+      };
+      
+   }
+}
+
+#endif  // Analysis_Tools_Utils_h
