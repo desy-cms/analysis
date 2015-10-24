8d0587f1442c8521d960bb8f53ddf7e085a203d2
Roberval Walsh <roberval.walsh@gmail.com>
2015-10-24 19:35:26 +0200

    Tests modifying Collection for matching

--- Analysis/Tools/bin/SimpleAnalysis.cc
+++ Analysis/Tools/bin/SimpleAnalysis.cc
@@ -24,24 +24,63 @@ int main(int argc, char * argv[])
    Analysis analysis(inputList);
    
    analysis.addTree<Jet> ("Jets","MssmHbb/Events/slimmedJetsPuppi");
-   analysis.addTree<Jet> ("Jets2","MssmHbb/Events/slimmedJets");
-   analysis.addTree<Muon>("Muons","MssmHbb/Events/slimmedMuons");
-   analysis.addTree<MET> ("METs","MssmHbb/Events/slimmedMETsPuppi"); 
-   analysis.addTree<Vertex> ("Vertices","MssmHbb/Events/offlineSlimmedPrimaryVertices"); 
+   analysis.addTree<TriggerObject> ("hltDoubleJetsC100","MssmHbb/Events/selectedPatTrigger/hltDoubleJetsC100");
+    analysis.addTree<TriggerObject> ("hltDoublePFJetsC100","MssmHbb/Events/selectedPatTrigger/hltDoublePFJetsC100");
+//    analysis.addTree<Muon>("Muons","MssmHbb/Events/slimmedMuons");
+//    analysis.addTree<MET> ("METs","MssmHbb/Events/slimmedMETsPuppi"); 
+//    analysis.addTree<Vertex> ("Vertices","MssmHbb/Events/offlineSlimmedPrimaryVertices"); 
    
    // Analysis of events
-   for ( int i = 0 ; i < analysis.size() ; ++i )
+   std::cout << "This analysis has " << analysis.size() << " events" << std::endl;
+//   for ( int i = 0 ; i < analysis.size() ; ++i )
+   for ( int i = 0 ; i < 1 ; ++i )
    {
       analysis.event(i);
       Collection<Jet> jets = analysis.collection<Jet>("Jets");
-      Collection<Jet> jets2 = analysis.collection<Jet>("Jets2");
-      Collection<Vertex> vtxs = analysis.collection<Vertex>("Vertices");
-      std::cout << "jets  " << jets.size() << "    "  << jets2.size() << std::endl;
-      for ( int i = 0 ; i < jets.size() ; ++i )
+      Collection<TriggerObject> hltDoubleJetsC100 = analysis.collection<TriggerObject>("hltDoubleJetsC100");
+      Collection<TriggerObject> hltDoublePFJetsC100 = analysis.collection<TriggerObject>("hltDoublePFJetsC100");
+      
+      jets.matchTo(hltDoubleJetsC100);
+      jets.matchTo(hltDoublePFJetsC100);
+      
+//       Collection<Vertex> vtxs = analysis.collection<Vertex>("Vertices");
+      for ( int j = 0 ; j < jets.size() ; ++j )
       {
-         Jet jet = jets.at(i);
-         jet.matchTo(vtxs.vectorCandidates(),"Vertices");
+         Jet jet = jets.at(j);
+//          jet.matchTo(hltDoubleJetsC100.vectorCandidates(),"hltDoubleJetsC100");
+//          jet.matchTo(hltDoublePFJetsC100.vectorCandidates(),"hltDoublePFJetsC100");
+         const Candidate * to_calojet = jet.matched("hltDoubleJetsC100");
+         const Candidate * to_pfjet = jet.matched("hltDoublePFJetsC100");
+         std::cout << "Jet " << j << "   " << jet.eta() << "   " << jet.phi() << "   " << std::endl;
+         if ( to_calojet )
+          {
+             std::cout << "SimpleAnalysis  TriggerObject CaloJet: " << to_calojet -> eta() << ", " << to_calojet -> phi() << "   " << to_calojet << std::endl;
+          }
+          if ( to_pfjet )
+          {
+             std::cout << "SimpleAnalysis  TriggerObject PFJet:   " << to_pfjet -> eta() << ", " << to_pfjet -> phi() << "   " << to_pfjet << std::endl;
+          }
       }
+      std::cout << "==============" << std::endl;
+      
+//       jets.matchTo(hltDoublePFJetsC100);
+//       for ( int j = 0 ; j < jets.size() ; ++j )
+//       {
+//          Jet jet = jets.at(j);
+// //          jet.matchTo(hltDoubleJetsC100.vectorCandidates(),"hltDoubleJetsC100");
+// //          jet.matchTo(hltDoublePFJetsC100.vectorCandidates(),"hltDoublePFJetsC100");
+//          const Candidate * to_calojet = jet.matched("hltDoubleJetsC100");
+//          const Candidate * to_pfjet = jet.matched("hltDoublePFJetsC100");
+//          std::cout << "Jet " << j << "   " << jet.eta() << "   " << jet.phi() << "   " << std::endl;
+//          if ( to_calojet )
+//           {
+//              std::cout << "SimpleAnalysis  TriggerObject CaloJet: " << to_calojet -> eta() << ", " << to_calojet -> phi() << "   " << to_calojet << std::endl;
+//           }
+//           if ( to_pfjet )
+//           {
+//              std::cout << "SimpleAnalysis  TriggerObject PFJet:   " << to_pfjet -> eta() << ", " << to_pfjet -> phi() << "   " << to_pfjet << std::endl;
+//           }
+//       }
        
 //       Collection<Muon> muons = analysis.collection<Muon>("Muons");
 //       std::cout << "muons  " << muons.size() << std::endl;
@@ -63,13 +102,13 @@ int main(int argc, char * argv[])
 //      std::cout << "=====" << std::endl;
    }
    
-   // cross sections
-   analysis.crossSections("MssmHbb/Metadata/CrossSections");
-   analysis.listCrossSections();
-   
-   //generator filter
-   analysis.generatorFilter("MssmHbb/Metadata/GeneratorFilter");
-   analysis.listGeneratorFilter();
+//    // cross sections
+//    analysis.crossSections("MssmHbb/Metadata/CrossSections");
+//    analysis.listCrossSections();
+//    
+//    //generator filter
+//    analysis.generatorFilter("MssmHbb/Metadata/GeneratorFilter");
+//    analysis.listGeneratorFilter();
    
       
 //    
