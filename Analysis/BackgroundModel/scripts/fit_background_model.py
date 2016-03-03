788f1eadbf246ea6c23a5c3803b3eea93e5ddf4e
Gregor Mittag <Gregor.Mittag@cern.ch>
2016-03-03 18:29:57 +0100

    Added infrastructure for comprehensive background model studies.

---
+++ Analysis/BackgroundModel/scripts/fit_background_model.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+
+import sys
+import ast
+import fcntl
+import argparse
+import subprocess
+import time
+import datetime
+
+
+################################################################################
+def main(argv=None):
+    """
+    Main routine which is not called, if this module is loaded via `import`.
+
+    Arguments:
+    - `argv`: command line arguments passed to the script
+    """
+
+    if argv == None:
+        argv = sys.argv[1:]
+
+    parser = argparse.ArgumentParser(
+        description = "Perform background model studies.")
+    parser.add_argument("--list", dest = "list", required = True,
+                        help = ("Python list representation containing the "
+                                "options to be passed to 'FitBackgroundModel'."))
+    parser.add_argument("--success", dest = "success", required = True,
+                        help = "Target file containing successful configs.")
+    parser.add_argument("--fail", dest = "fail", required = True,
+                        help = "Target file containing failed configs.")
+    args = parser.parse_args(argv)
+
+    arguments_list = ast.literal_eval(args.list)
+
+    if len(arguments_list) > 0 and type(arguments_list[0]) not in (tuple, list):
+        FitBackgroundModel(args.success, args.fail, *arguments_list)
+    else:
+        map(lambda x: FitBackgroundModel(args.success, args.fail, *x),
+            arguments_list)
+
+
+
+
+################################################################################
+def FitBackgroundModel(success, fail, *args):
+    """
+    Python wrapper for C++ executable 'FitBackgroundModel'.
+    
+    Arguments:
+    - `success`: bookkeeping file for successful configurations
+    - `fail`: bookkeeping file for failed configurations
+    - `*args`: arguments for 'FitBackgroundModel'
+    """
+
+    cmd = ("FitBackgroundModel",) + args
+    msg = "Calling: `" + " ".join(cmd) + "`"
+    print "\n"
+    print "*"*(len(msg)+6)
+    print "*" + " "*(len(msg)+4) + "*"
+    print "*  " + msg + "  *"
+    print "*" + " "*(len(msg)+4) + "*"
+    print "*"*(len(msg)+6)
+    print
+    sys.stdout.flush()
+    start = datetime.datetime.now()
+    p = subprocess.Popen(("FitBackgroundModel",) + args)
+    while True:
+        # wait at most for 30 minutes to let the process finish:
+        if p.poll() == None:
+            if (datetime.datetime.now() - start).seconds > 1800:
+                print "Process killed because it took longer than 30 minutes."
+                p.kill()
+                with open(fail, "a") as f:
+                    fcntl.flock(f, fcntl.LOCK_EX)
+                    f.write(args.__repr__()+"\n")
+                    fcntl.flock(f, fcntl.LOCK_UN)
+                break
+            else:               
+                time.sleep(10)  # wait for 30 seconds to poll again
+        else:
+            break
+    if p.poll() == 0:
+        with open(success, "a") as f:
+            fcntl.flock(f, fcntl.LOCK_EX)
+            f.write(args.__repr__()+"\n")
+            fcntl.flock(f, fcntl.LOCK_UN)
+    else:
+        with open(fail, "a") as f:
+            fcntl.flock(f, fcntl.LOCK_EX)
+            f.write(args.__repr__()+"\n")
+            fcntl.flock(f, fcntl.LOCK_UN)
+
+
+################################################################################
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
---
+++ Analysis/BackgroundModel/scripts/submit_background_model_studies.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+
+import os
+import os.path as op
+import sys
+import argparse
+import subprocess
+
+
+################################################################################
+def main(argv=None):
+    """
+    Main routine which is not called, if this module is loaded via `import`.
+
+    Arguments:
+    - `argv`: command line arguments passed to the script
+    """
+
+    if argv == None:
+        argv = sys.argv[1:]
+
+    parser = argparse.ArgumentParser(
+        description = "Perform background model studies.")
+    parser.add_argument("-o", "--output-dir", dest = "output_dir",
+                        metavar = "PATH", required = True,
+                        help = "Path where output is stored.")
+    args = parser.parse_args(argv)
+
+    job_configs = create_job_configs(args.output_dir)
+    split_configs = split_batch_jobs(job_configs, 5)
+    submit_batch_jobs(split_configs, args.output_dir)
+
+    
+################################################################################
+def submit_batch_jobs(splitted_configs, out_dir):
+    """
+    Submit batch jobs to the NAF.
+    
+    Arguments:
+    - `splitted_configs`: list of configuration lists
+    """
+
+    submit_dir = op.join(out_dir, "submit")
+    
+    try:
+        os.makedirs(submit_dir)
+    except OSError as e:
+        if e.args == (17, "File exists"):
+            pass
+        else:
+            raise
+
+    i = 0
+    for configs in splitted_configs:
+        script = script_template.format(arguments = configs.__repr__(),
+                                        success = op.join(out_dir, "success.txt"),
+                                        fail = op.join(out_dir, "fail.txt"))
+        file_name = op.join(submit_dir,
+                            "submit_background_model_studies_"+
+                            str(i).zfill(4)+".sh")
+        with open(file_name, "w") as f:
+            f.write(script)
+        subprocess.call(["qsub", file_name], cwd = submit_dir)
+        i += 1
+
+
+def split_batch_jobs(configs, size):
+    """
+    Splits into lists of `size` job configurations.
+    
+    Arguments:
+    - `configs`: unsplitted job configuration list
+    - `size`: size of the created batch job configuration lists
+    """
+    
+    for i in xrange(0, len(configs), size):
+        yield configs[i:i+size]
+
+
+def create_job_configs(out_dir):
+    """
+    Creates job configurations.
+
+    Arguments:
+    - `out_dir`: path to the output location
+    """
+
+    data_dir = op.join(os.environ["CMSSW_BASE"], "src", "Analysis",
+                       "BackgroundModel", "data")
+
+    inputs = {"8TeV":  ["--input_file",
+                        op.join(data_dir, "HIG14017_HighMass2012_Packed_M350_inputs.root")],
+              "13TeV": ["--input_tree_file",
+                        op.join(data_dir, "DoubleBTagSelection_13TeV.root")]}
+    models = {"polynomials": ["bernstein", "berneffprod"], #"Chebychev"
+              "non-polynomials": ["bukin", "crystalball", "novosibirsk"]}
+    fit_min = [str(x) for x in xrange(0, 301, 10)]
+    orders = [str(x) for x in xrange(3, 20)]
+
+    configs = []
+    for inp in inputs:
+        for model_type in models:
+            for model in models[model_type]:
+                for mbb in fit_min:
+                    if model_type == "polynomials":
+                        for order in orders:
+                            config = ["-b", model+","+order,
+                                      "-o", op.join(out_dir, inp, "_".join([model, order, mbb])),
+                                      # "-p",
+                                      "--fit_min", mbb]
+                            config.extend(inputs[inp])
+                            configs.append(config)
+                    else:
+                        config = ["-b", model,
+                                  "-o", op.join(out_dir, inp, "_".join([model, mbb])),
+                                  # "-p",
+                                  "--fit_min", mbb]
+                        config.extend(inputs[inp])
+                        configs.append(config)
+
+    return configs
+
+
+################################################################################
+script_template="""\
+#!/bin/zsh
+#$ -S /bin/zsh
+#$ -l h_rt=02:59:59
+#$ -j y
+#$ -cwd
+#$ -l site=hh
+#$ -V
+#$ -M {user:s}@mail.desy.de
+#$ -m ae
+
+echo "Job started at: $(date)"
+fit_background_model.py --success {{success:s}} --fail {{fail:s}} --list "{{arguments:s}}"
+echo "Job ended at: $(date)"
+""".format(user = os.environ["USER"])
+
+
+################################################################################
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
