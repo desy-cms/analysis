d024d2e261824f7555b346142d43ed8f28f84667
Roberval Walsh <roberval.walsh@gmail.com>
2016-07-12 15:09:22 +0200

    Outdated

--- Analysis/Ntuplizer/test/submitCrab3.py
+++ Analysis/Ntuplizer/test/submitCrab3.py
@@ -7,12 +7,12 @@ from WMCore.Configuration import Configuration
 
 # ---
 # Some parameter steering
-PROCESS         = 'TTJets'
-UNITS_PER_JOB   = 1
+PROCESS         = 'QCD_pythia8_1'
+UNITS_PER_JOB   = 2
 TYPE            = 'MC'
-PSET            = 'ntuplizer_mc_76x_expert_v2.py'
-CAMPAIGN        = 'Fall15.pro-ntuple-2015'
-BASEOUTDIR      = '/store/user/rwalsh/Analysis/Ntuples/' + CAMPAIGN + '/'+ PROCESS
+PSET            = 'ntuplizer_mc_76x_summer_conferences_2016_v1.py'
+CAMPAIGN        = 'Fall15/76x_summer_conferences_2016_v1'
+BASEOUTDIR      = '/store/user/rwalsh/Analysis/Ntuples/' + TYPE + '/' + CAMPAIGN
 URL             = 'http://www.desy.de/~walsh/cms/analysis/samples/miniaodsim/Fall15'
 
 # ---
@@ -37,22 +37,31 @@ if __name__ == '__main__':
       config.Data.splitting   = 'LumiBased'
 #      config.JobType.psetName = 'ntuplizer_data.py'
       
-   config.General.workArea += '_' + PROCESS
-   config.Data.unitsPerJob = UNITS_PER_JOB
+#   config.General.workArea += '_' + PROCESS
+
+# If dataset not valid yet, will run over valid files only
+#   config.Data.allowNonValidInputDataset = True
    
    for dataset in datasets:
       dataset = dataset.split('\n')[0]
       dataset_name = dataset.split('/')[1]
       dataset_cond = dataset.split('/')[2]
       dataset_tier = dataset.split('/')[3]
+#      
       config.Data.inputDataset    = dataset
+#      config.Data.inputDBS = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSReader/'
       config.Data.outputDatasetTag = dataset_cond
-      config.Data.unitsPerJob  = 10
+      config.Data.unitsPerJob  = UNITS_PER_JOB
       config.Data.totalUnits   = -1
+      config.Data.outLFNDirBase   = BASEOUTDIR + '/'
+#      
+# use if needed in private productions (modify accordingly)
       config.General.requestName  = dataset_name
-      config.Data.outLFNDirBase   = BASEOUTDIR + '/' + dataset_tier + '/'
+# use if needed in private productions (modify accordingly)
+#      processname = dataset_cond.split('_')
+#      config.General.requestName  += '_'+processname[0]+'-'+processname[1]+'_oldGT'
+#      
       config.JobType.psetName    = PSET
-      config.JobType.outputFiles = ['ntuple_mc_76x_expert_v2.root']
       config.JobType.inputFiles = ['Fall15_25nsV2_MC_PtResolution_AK4PFPuppi.txt','Fall15_25nsV2_MC_PtResolution_AK4PFchs.txt','Fall15_25nsV2_MC_SF_AK4PFPuppi.txt','Fall15_25nsV2_MC_SF_AK4PFchs.txt']
       crabCommand('submit', config = config)
 
