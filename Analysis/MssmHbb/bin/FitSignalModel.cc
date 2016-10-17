7a4362438b425a28dd48246dd5a33d180e7052a5
Rostyslav Shevchenko <shevchenko.rostislav@gmail.com>
2016-10-17 13:40:03 +0200

    New binary to prepare signal workspace for parametric fit.

--- Analysis/MssmHbb/bin/FitSignalModel.cc
+++ Analysis/MssmHbb/bin/FitSignalModel.cc
@@ -1,4 +1,7 @@
 #include <vector>
+#include <array>
+#include <map>
+
 #include "stdlib.h"
 #include <memory>
 #include <iostream>
@@ -8,17 +11,31 @@
 #include "TMatrixD.h"
 #include "TMatrixDSym.h"
 #include "TMatrixDSymEigen.h"
+
+#include "TFormula.h"
+
+#include "RooDataHist.h"
+#include "RooFormulaVar.h"
+#include "RooRealVar.h"
 #include "RooFitResult.h"
 #include "RooPlot.h"
 #include "RooWorkspace.h"
 
 #include "TFile.h"
+#include "TLegend.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TAxis.h"
+
 #include "Analysis/BackgroundModel/interface/HistContainer.h"
 #include "Analysis/BackgroundModel/interface/FitContainer.h"
 #include "Analysis/BackgroundModel/interface/ParamModifier.h"
 #include "Analysis/BackgroundModel/interface/Tools.h"
 #include "Analysis/BackgroundModel/interface/ProbabilityDensityFunctions.h"
+#include "Analysis/BackgroundModel/interface/RooQuadGausExp.h"
+#include "Analysis/MssmHbb/interface/utilLib.h"
 
+// hi my name is Rostyslav and I like bananas
 
 namespace po = boost::program_options;
 namespace ab = analysis::backgroundmodel;
@@ -26,7 +43,10 @@ namespace ab = analysis::backgroundmodel;
 typedef std::unique_ptr<TFile> pTFile;
 
 using namespace analysis::backgroundmodel;
+using namespace std;
 
+void drawSystFits(const int & mass,const std::string &syst, const std::string &output);
+void plotShapeParameters(const int & mass,const vector<string> &syst, const std::string &folder);
 
 int main(int argc, char* argv[]) {
   const auto cmsswBase = static_cast<std::string>(gSystem->Getenv("CMSSW_BASE"));
@@ -38,74 +58,164 @@ int main(int argc, char* argv[]) {
 	signal[1300]= "/src/Analysis/MssmHbb/output/MssmHbbSignal_lowM_clange-SUSYGluGluToBBHToBB_M-1300_cfg_GEN_DIGI76X_RECO76X_MiniAODv2_76X-17d438ff51ec6b3cada9e499a5a978e0.root";
 	signal[500] = "/src/Analysis/MssmHbb/output/MssmHbbSignal_lowM_clange-SUSYGluGluToBBHToBB_M-500_cfg_GEN_DIGI76X_RECO76X_MiniAODv2_76X-17d438ff51ec6b3cada9e499a5a978e0.root";
 	signal[600] = "/src/Analysis/MssmHbb/output/MssmHbbSignal_lowM_clange-SUSYGluGluToBBHToBB_M-600_cfg_GEN_DIGI76X_RECO76X_MiniAODv2_76X-17d438ff51ec6b3cada9e499a5a978e0.root";
-	signal[200] = "/src/Analysis/MssmHbb/output/MssmHbbSignal_lowM_SUSYGluGluToBBHToBB_M-200_TuneCUETP8M1_13TeV-pythia8.root";
+//	signal[200] = "/src/Analysis/MssmHbb/output/MssmHbbSignal_lowM_SUSYGluGluToBBHToBB_M-200_TuneCUETP8M1_13TeV-pythia8.root";
 	signal[250] = "/src/Analysis/MssmHbb/output/MssmHbbSignal_lowM_SUSYGluGluToBBHToBB_M-250_TuneCUETP8M1_13TeV-pythia8.root";
 	signal[300] = "/src/Analysis/MssmHbb/output/MssmHbbSignal_lowM_SUSYGluGluToBBHToBB_M-300_TuneCUETP8M1_13TeV-pythia8.root";
 	signal[350] = "/src/Analysis/MssmHbb/output/MssmHbbSignal_lowM_SUSYGluGluToBBHToBB_M-350_TuneCUETP8M1_13TeV-pythia8.root";
 	signal[400] = "/src/Analysis/MssmHbb/output/MssmHbbSignal_lowM_SUSYGluGluToBBHToBB_M-400_TuneCUETP8M1_13TeV-pythia8.root";
-
-  const int verbosity = 6;
+/**/
+  const int verbosity = 2;
   std::string histo_name = "bbH_Mbb";
-
+  //List of shape uncertainties
+  std::vector<std::string> shape_unc = {"JER","JES"};
+  std::string CMS = "CMS";
+  std::string energy = "13TeV";
+  std::array<std::string,2> sign = { {"Up","Down"} };
+  std::string model = "quadgausexp";
   for(const auto & mass : signal){
 	  TFile f((cmsswBase + mass.second).c_str(),"read");
-//	  TH1D& hSignal = (TH1D&) f.Get(histo_name.c_str());
 	  ab::HistContainer histContainer(cmsswBase + mass.second);//MssmHbbSignal_lowM_SUSYGluGluToBBHToBB_M-400_TuneCUETP8M1_13TeV-pythia8.root");
 	  histContainer.Rebin(1);
-	  ab::FitContainer fitter(histContainer,cmsswBase + "/src/Analysis/MssmHbb/test/" + "mass_point_" + std::to_string(mass.first));
-//	  ab::FitContainer fitter(hSignal,cmsswBase + "/src/Analysis/MssmHbb/test/" + "mass_point_test" + std::to_string(mass.first));
-	  fitter.verbosity(verbosity - 1);
-
-	  std::string model = "quadgausexp";//"quadgausexp";
-	  fitter.setModel(ab::FitContainer::Type::signal,model);
-	//  fitter.profileModel(ab::FitContainer::Type::signal);
-
-	  std::unique_ptr<RooFitResult> Signalfit = fitter.FitSignal(model);
+	  std::string name = cmsswBase + "/src/Analysis/MssmHbb/output/" + "mass_point_" + std::to_string(mass.first);
+	  std::string hname = histo_name;
+	  //Central value
+	  TH1D* hSignal = (TH1D*) f.Get(histo_name.c_str());
+          if(hSignal == nullptr) throw std::logic_error("Wrong histo name: " + histo_name + " has been provided");
+          ab::FitContainer fitter(hSignal,name,"signal");
+          fitter.verbosity(verbosity - 1);
+          fitter.setModel(ab::FitContainer::Type::signal,model);
+          std::unique_ptr<RooFitResult> Signalfit = fitter.FitSignal(model);
+    	  RooRealVar signal_norm("signal_norm","signal_norm",hSignal->Integral());
+    	  signal_norm.setConstant(true);
+    	  fitter.Import(signal_norm);
+    	  fitter.Write();
+	  //Syst variation 
+	  for(const auto & syst : shape_unc){
+		for(const auto & s : sign){
+               	hname = histo_name + "_" + CMS + "_" + syst + "_" + energy + s;
+               	TH1D* hSignal = (TH1D*) f.Get(hname.c_str());
+               	if(hSignal == nullptr) throw std::logic_error("Wrong histo name: " + hname + " has been provided");
+               	ab::FitContainer fit_syst(hSignal,name + "/" + syst + "_" + s,"signal");
+               	fit_syst.verbosity(verbosity - 1);
+               	fit_syst.setModel(ab::FitContainer::Type::signal,model);
+               	std::unique_ptr<RooFitResult> Signalfit_syst = fit_syst.FitSignal(model);
+               	RooRealVar signal_norm("signal_norm","signal_norm",hSignal->Integral());
+               	signal_norm.setConstant(true);
+               	fit_syst.Import(signal_norm);
+               	fit_syst.Write();
+	        }
+		//Draw plots
+		drawSystFits(mass.first,syst,name);
+	  }
+	  //Plot shape parameters
+	  plotShapeParameters(mass.first,shape_unc,name);
   }
 
-  /*
-  ab::HistContainer histContainer(cmsswBase + signal.at(0));//MssmHbbSignal_lowM_SUSYGluGluToBBHToBB_M-400_TuneCUETP8M1_13TeV-pythia8.root");
-  histContainer.Rebin(1);
-  ab::FitContainer fitter(histContainer,cmsswBase + "/src/Analysis/MssmHbb/test");
-  fitter.verbosity(verbosity - 1);
-
-  std::string model = "quadgausexp";//"quadgausexp";
-  fitter.setModel(ab::FitContainer::Type::signal,model);
-//  fitter.profileModel(ab::FitContainer::Type::signal);
-
-  std::unique_ptr<RooFitResult> Signalfit = fitter.FitSignal(model);
-
-  /*
-  if (bkgOnlyFit) {
-    std::cout << "\nCorrelation matrix of background-only fit:" << std::endl;
-    if (&(bkgOnlyFit->correlationMatrix()) != nullptr) {
-      bkgOnlyFit->correlationMatrix().Print("v");
-
-      TMatrixDSymEigen CM = bkgOnlyFit->correlationMatrix();
-      TMatrixD CM_eigen = CM.GetEigenVectors();
-      std::cout << "Eigenvectors of Correlation Matrix" << std::endl;
-      CM_eigen.Print("v");
-
-    } else {
-      std::cout << ">>> no correlation matrix available" << std::endl;
-      returnValue = 1;
-    }
-    if (&(bkgOnlyFit->covarianceMatrix()) != nullptr) {
-      std::cout << "Covariance Matrix" << std::endl;
-      bkgOnlyFit->covarianceMatrix().Print("v");
-
-      TMatrixDSymEigen VM = bkgOnlyFit->covarianceMatrix();
-      TMatrixD VM_eigen = VM.GetEigenVectors();
-      std::cout << "Eigenvectors of Covariance Matrix" << std::endl;
-      VM_eigen.Print("v");
-    } else {
-      std::cout << ">>> no covariance matrix available" << std::endl;
-      returnValue = 1;
-    }
-  } else {
-    returnValue = 1;
-  } */
-
   return 0;
+
+}
+
+void drawSystFits(const int & mass,const std::string &syst, const std::string &output){
+	TFile fCentral( (output + "/workspace/FitContainer_workspace.root").c_str(),"read");
+	TCanvas c("c","c",1000,800);
+	RooWorkspace& wCentral = *( (RooWorkspace*) fCentral.Get("workspace"));
+
+	//Extract RooRealVar for X-axis
+	RooRealVar& x = *( wCentral.var("mbb"));
+	RooPlot& frame = *(x.frame());
+	RooAbsPdf& PdfCentral = *(wCentral.pdf("signal"));
+	PdfCentral.plotOn(&frame,RooFit::LineColor(kBlack),RooFit::Name("Central"));
+
+	//down:
+	TFile fDown( (output + "/" + syst + "_" + "Down/workspace/FitContainer_workspace.root").c_str());
+	RooWorkspace& wDown = *( (RooWorkspace*) fDown.Get("workspace"));
+	RooAbsPdf& PdfDown = *(wDown.pdf("signal"));
+	PdfDown.plotOn(&frame,RooFit::LineColor(kBlue),RooFit::Name("Down"));
+
+	//up
+	TFile fUp( (output + "/" + syst + "_" + "Up/workspace/FitContainer_workspace.root").c_str());
+	RooWorkspace& wUp = *( (RooWorkspace*) fUp.Get("workspace"));
+	RooAbsPdf& PdfUp = *(wUp.pdf("signal"));
+	PdfUp.plotOn(&frame,RooFit::LineColor(kRed),RooFit::Name("Up"));
+
+//	TPad pad1("pad1","",0.,0.35,1,1);
+//	pad1.SetBottomMargin(0.0);
+//	pad1.SetLeftMargin(0.16);
+//	pad1.SetRightMargin(0.05);
+//	pad1.Draw();
+//	pad1.cd();
+
+	TLegend leg(0.6,0.7,0.9,0.9);
+	frame.Draw();
+	leg.AddEntry(frame.findObject("Central"),"Central","l");
+	leg.AddEntry(frame.findObject("Down"),("-2#sigma variation " + syst).c_str(),"l");
+	leg.AddEntry(frame.findObject("Up"),("+2#sigma variation " + syst).c_str(),"l");
+	leg.Draw();
+
+//	c.cd();
+//	TPad pad2("pad2","",0.,0.21,1,0.35);
+//	pad2.SetBottomMargin(0.0);
+//	pad2.SetTopMargin(0.);
+//	pad2.SetLeftMargin(pad1.GetLeftMargin());
+//	pad2.SetRightMargin(pad1.GetRightMargin());
+//	pad2.Draw();
+//	pad2.cd();
+
+	c.Print( (output + "/plots/" + syst + ".png").c_str());
+}
+
+void plotShapeParameters(const int & mass,const vector<string> &syst, const std::string &folder){
+	TFile fCentral( (folder + "/workspace/FitContainer_workspace.root").c_str(),"read");
+	RooWorkspace& wCn = *( (RooWorkspace*) fCentral.Get("workspace"));
+
+	for(const auto& s: syst){
+		//down
+		TFile fDown( (folder + "/" + s + "_" + "Down/workspace/FitContainer_workspace.root").c_str());
+		RooWorkspace& wDn = *( (RooWorkspace*) fDown.Get("workspace"));
+		//up
+		TFile fUp( (folder + "/" + s + "_" + "Up/workspace/FitContainer_workspace.root").c_str());
+		RooWorkspace& wUp = *( (RooWorkspace*) fUp.Get("workspace"));
+
+		//Signal shape parameters list
+		vector<string> parameters = {"signal_norm","mean","sigmaL1","sigmaR1","sigmaL2","sigmaR2","tail_shift","tail_sigma","norm_g1","norm_g2"};
+		TCanvas c(("c" + s).c_str(),"c",1000,800);
+		TH1F hUp("hUp",(s + ";Shape parameters;(#theta_{up/dn} - #theta_{0})/#theta_{0}").c_str(),parameters.size(),0,10);
+		TH1F hDn("hDn","hDn",parameters.size(),0,10);
+		hDn.SetCanExtend(TH1::kAllAxes);
+		hUp.SetCanExtend(TH1::kAllAxes);
+		hUp.SetStats(0);
+		hUp.SetMarkerStyle(21);
+		hUp.SetMarkerColor(kRed);
+		hDn.SetMarkerStyle(21);
+		hDn.SetMarkerColor(kBlue);
+		hUp.GetYaxis()->SetRangeUser(-1.,1.);
+		int i = 0;
+		double e_up;
+		double e_down;
+		for(const auto& p : parameters){
+			++i;
+			double up = (wUp.var(p.c_str())->getValV() - wCn.var(p.c_str())->getValV()) / wCn.var(p.c_str())->getValV();
+			if(wUp.var(p.c_str())->getError() != 0) e_up = wUp.var(p.c_str())->getValV()/wCn.var(p.c_str())->getValV() * sqrt( pow(wUp.var(p.c_str())->getValV()/wUp.var(p.c_str())->getError(),2) + pow(wCn.var(p.c_str())->getValV()/wCn.var(p.c_str())->getError(),2) );
+			else e_up = 0;
+			double down = (wDn.var(p.c_str())->getValV() - wCn.var(p.c_str())->getValV()) /wCn.var(p.c_str())->getValV();
+			if(wDn.var(p.c_str())->getError() != 0) e_down = wDn.var(p.c_str())->getValV()/wCn.var(p.c_str())->getValV() * sqrt( pow(wDn.var(p.c_str())->getValV()/wDn.var(p.c_str())->getError(),2) + pow(wCn.var(p.c_str())->getValV()/wCn.var(p.c_str())->getError(),2) );
+			else e_down = 0;
+			cout<<p<<" val: "<<up<<" +/- "<<e_up<<" "<<down<<" +/- "<<e_down<<endl;
+			hUp.GetXaxis()->SetBinLabel(i,p.c_str());
+			hUp.SetBinContent(i,up);
+			hUp.SetBinError(i,e_up);
+			hDn.SetBinContent(i,down);
+			hDn.SetBinError(i,e_down);
+		}
+		hUp.Draw("E");
+		hDn.Draw("Esame");
+		TLegend leg(0.65,0.6,0.85,0.8);
+		leg.SetLineColor(0);
+		leg.AddEntry(&hUp,"Up","p");
+		leg.AddEntry(&hDn,"Down","p");
+		leg.Draw();
+
+		c.Print( (folder + "/plots/delta" + s + ".pdf").c_str() );
+	}
 
 }
---
+++ Analysis/MssmHbb/bin/prepParamWorkspace.cpp
@@ -0,0 +1,219 @@
+#include "boost/program_options.hpp"
+
+#include <exception>
+#include <iostream>
+#include <string>
+#include <memory>
+
+#include <vector>
+
+//Root includes
+#include "TH1.h"
+#include "TFile.h"
+#include "TSystem.h"
+#include "TDirectory.h"
+
+//RooFit includes
+#include "RooWorkspace.h"
+#include "RooRealVar.h"
+#include "RooAbsPdf.h"
+#include "RooDataHist.h"
+
+#include "Analysis/BackgroundModel/interface/RooQuadGausExp.h"
+
+namespace
+{
+  const size_t ERROR_IN_COMMAND_LINE = 1;
+  const size_t SUCCESS = 0;
+  const size_t ERROR_UNHANDLED_EXCEPTION = 2;
+  const size_t ERROR_BG_WORKSPACE = 3;
+  const size_t ERROR_SIGNAL_WORKSPACE = 4;
+} // namespace
+
+using namespace std;
+using namespace boost::program_options;
+
+//defin >> stream operator to be used in boost::po
+namespace std {
+	istream& operator>>(istream& in, pair<string,string>& ss) {
+	string s;
+  	  in >> s;
+  	  const size_t sep = s.find(':');
+  	  if(sep ==string::npos) s.find(',');
+  	  if (sep==string::npos) {
+	  ss.first = string();
+    	ss.second = s;
+  	  } else {
+	  ss.first  = s.substr(0,sep);
+    	ss.second = s.substr(sep+1);
+  	  }
+  	  return in;
+	}
+}
+
+typedef unique_ptr<TFile> pTFile;
+typedef map<string, map<string, pair<string,string> > > myMap;
+
+void setup_bg(const string& in_path,const string& out_path, TH1& data_obs);
+void setup_signal(const string& in_path, const vector<string>& syst);
+
+int main(int argc, char ** argv){
+	try {
+
+		//list of the inputs:
+		const string cmsswBase = getenv("CMSSW_BASE");
+		string input_background;
+		string data_obs = cmsswBase + "/src/Analysis/MssmHbb/output/MssmHbbSignal_lowM_clange-SUSYGluGluToBBHToBB_M-600_cfg_GEN_DIGI76X_RECO76X_MiniAODv2_76X-17d438ff51ec6b3cada9e499a5a978e0.root";
+		vector<string> input_signal = {
+				cmsswBase + "/src/Analysis/MssmHbb/output/mass_point_250",
+				cmsswBase + "/src/Analysis/MssmHbb/output/mass_point_300",
+				cmsswBase + "/src/Analysis/MssmHbb/output/mass_point_350",
+				cmsswBase + "/src/Analysis/MssmHbb/output/mass_point_400",
+				cmsswBase + "/src/Analysis/MssmHbb/output/mass_point_500",
+				cmsswBase + "/src/Analysis/MssmHbb/output/mass_point_600",
+				cmsswBase + "/src/Analysis/MssmHbb/output/mass_point_700",
+				cmsswBase + "/src/Analysis/MssmHbb/output/mass_point_900",
+				cmsswBase + "/src/Analysis/MssmHbb/output/mass_point_1100",
+				cmsswBase + "/src/Analysis/MssmHbb/output/mass_point_1300",
+								};
+		vector<string> systematics = {"JES","JER"};
+		string output = cmsswBase + "/src/Analysis/MssmHbb/output/bg_fit/workspace/background_workspace.root";
+
+		options_description desc("Options");
+		desc.add_options()
+				("help,h", "Print help messages")
+				("background,b", value<string>(&input_background)->default_value(cmsswBase + "/src/Analysis/MssmHbb/output/bg_fit/workspace/bg.root"), "Input background workspace")
+				("signal,s", value<vector<string> >(&input_signal)->multitoken(),"Input std::vector<signal folder>" )
+				("data_obs,d", value<string>(&data_obs),"Input file with observed data")
+				("syst_errors,e",value<vector<string> >(&systematics)->multitoken(),"std::vector<systematic error>" )
+				("output,o", value<string>(&output),"Output for background file.root" )
+				;
+
+		//Boost container for input arguments.
+		variables_map vm;
+		try {
+			store(parse_command_line(argc,argv,desc), vm);
+
+			// --help option
+			if(vm.count("help")){
+				cout<<"Basic Command Line App to prepare signal/bg workspaces for parametric combine tool fitting"<<endl;
+				cout<<desc<<endl;
+				return SUCCESS;
+			}
+
+			notify(vm);
+		} catch (error& e) {
+			//Command line parcing can throw exception
+			// should be catched
+			cerr<<"ERROR: "<<e.what()<< endl << endl;
+			cerr<< desc;
+			return ERROR_IN_COMMAND_LINE;
+		}
+
+		//----------Main procedure----------
+		TFile temp(data_obs.c_str(),"read");
+		//Adjust bg workspace
+		try {
+			setup_bg(input_background,output,*((TH1D*) temp.Get("data_obs")));
+		} catch (exception& e) {
+			cerr<<e.what()<<endl;
+			return ERROR_BG_WORKSPACE;
+		}
+
+		//Create signal workspace
+		for(const auto& in : input_signal){
+			try {
+				setup_signal(in,systematics);
+			} catch (exception& e) {
+				cerr<<e.what()<<endl;
+				return ERROR_SIGNAL_WORKSPACE;
+			}
+		}
+
+	} catch (exception& e) {
+		cerr<<"Unhandled Exception reached the top of main: "<<e.what()<<", application will now exit";
+		return ERROR_UNHANDLED_EXCEPTION;
+	}
+	return SUCCESS;
+}
+
+void setup_bg(const string& in_path,const string& out_path, TH1& data_obs){
+	if(gSystem->AccessPathName(in_path.c_str()) ) throw invalid_argument("Error: no bg file " + in_path);
+	//Read input file
+	TFile fIn(in_path.c_str(),"read");
+	//Input workspace
+	RooWorkspace& w = *( (RooWorkspace*)fIn.Get("workspace") );
+	//Prepare RooDataHist for data_obs;
+	RooRealVar mbb("mbb","mbb",data_obs.GetXaxis()->GetXmin(),data_obs.GetXaxis()->GetXmax());
+	RooDataHist data("data_obs","data_obs",mbb,RooFit::Import(data_obs));
+	//Prepare Bg normalisation variable WARNING - can be wrong!!!
+	string bg_pdf_name = "background";
+	if(w.pdf(bg_pdf_name.c_str()) == nullptr) throw invalid_argument("Error: no <background> pdf has been found in bg workspace");
+	RooRealVar bg_norm((bg_pdf_name+"_norm").c_str(),"background_norm",data_obs.Integral());
+
+	w.import(data);
+	w.import(bg_norm);
+	w.writeToFile(out_path.c_str());
+}
+
+void setup_signal(const string& in_folder, const vector<string>& syst){
+	//Sanity check of input folder
+	if(gSystem->AccessPathName(in_folder.c_str())) throw invalid_argument("Error in <prepParamWorkspace::setup_signal>: Wrong path: " + in_folder);
+	//Sanity check of central WP
+	string central = (in_folder + "/workspace/FitContainer_workspace.root");
+	if(gSystem->AccessPathName(central.c_str())) throw invalid_argument("Error in <prepParamWorkspace::setup_signal>: file /workspace/FitContainer_workspace.root was not found in " + in_folder);
+	//Read central workspace
+	TFile fInCentral( (in_folder + "/workspace/FitContainer_workspace.root").c_str(),"read");
+	RooWorkspace& wInCentral = *( (RooWorkspace*) fInCentral.Get("workspace") );
+	RooRealVar& mbb = *( (RooRealVar*) wInCentral.var("mbb") );
+	//Output workspace
+	RooWorkspace w("workspace");
+	//Signal shape parameters list
+	vector<string> parameters = {"signal_norm","mean","sigmaL1","sigmaR1","sigmaL2","sigmaR2","tail_shift","tail_sigma","norm_g1","norm_g2"};
+	//Form strings for RooFormulaVars
+	map<string,string> formula;
+	RooArgList nuisance_list; // Needed to add nuisances
+	for(const auto &par : parameters){
+		RooRealVar& vCent = (RooRealVar&) *wInCentral.var(par.c_str());
+		formula[par] = to_string(vCent.getValV()) + " + ";
+	}
+	//Loop over the system. sources
+	for(const auto& s : syst){
+		//Read Up and Down TFiles
+		string dnName = (in_folder + "/" + s + "_Up/workspace/FitContainer_workspace.root");
+		if(gSystem->AccessPathName(dnName.c_str()) ) throw invalid_argument("Error in <prepParamWorkspace::setup_signal>: No /" + s + "_Up/workspace/FitContainer_workspace.root in " + in_folder);
+		TFile fInUp( (dnName).c_str(),"read" );
+		string upName = in_folder + "/" + s + "_Down/workspace/FitContainer_workspace.root";
+		if(gSystem->AccessPathName(upName.c_str()) ) throw invalid_argument("Error in <prepParamWorkspace::setup_signal> No /" + s + "_Down/workspace/FitContainer_workspace.root in " + in_folder);
+		TFile fInDown( (upName).c_str(),"read");
+		//Get workspaces:
+		RooWorkspace& wInUp = *( (RooWorkspace*) fInUp.Get("workspace") );
+		RooWorkspace& wInDown = *( (RooWorkspace*) fInDown.Get("workspace") );
+		//RooRealVar for nuisance:
+		RooRealVar nuisance(s.c_str(),s.c_str(),0,-4,4);
+		for(const auto &par : parameters){
+			RooRealVar& vUp   = (RooRealVar&) *wInUp.var(par.c_str());
+			RooRealVar& vDown = (RooRealVar&) *wInDown.var(par.c_str());
+			string f = "("+ to_string(vUp.getValV()) + " - " + to_string(vDown.getValV()) +") * 0.25 * " + s + " ";
+			formula[par] += f;
+			if(s != syst.back()) formula[par] += "+ ";
+		}
+		w.import(nuisance);
+		nuisance_list.add(*w.var(nuisance.GetName()));
+	}
+	//Finaly make a RooFormulaVars:
+	for(const auto &par : parameters){
+		RooFormulaVar fPar( (par).c_str(),(par).c_str(),formula[par].c_str(),nuisance_list);
+		w.import(fPar);
+	}
+	//Create new RooQuadGausExp as a function of Nuisnace 1 : JES
+	analysis::backgroundmodel::RooQuadGausExp func("signal","signal",mbb,(RooFormulaVar&) *w.function("mean"), (RooFormulaVar&) *w.function("sigmaL1"),
+			(RooFormulaVar&) *w.function("sigmaL2"), (RooFormulaVar&) *w.function("sigmaR1"),
+			(RooFormulaVar&) *w.function("sigmaR2"), (RooFormulaVar&) *w.function("tail_shift"),
+			(RooFormulaVar&) *w.function("tail_sigma"), (RooFormulaVar&) *w.function("norm_g1"), (RooFormulaVar&) *w.function("norm_g2"));
+	w.import(func);
+	w.Print("v");
+	string out_path = in_folder + "/workspace/signal_workspace.root";
+	w.writeToFile(out_path.c_str());
+	cout<<"INFO: file " + out_path +" was created"<<endl;
+}
