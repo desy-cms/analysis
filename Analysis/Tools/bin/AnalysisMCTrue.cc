94b41bb538799a998d79f3cdc8e42a532200de47
Roberval Walsh <roberval.walsh@gmail.com>
2016-11-11 14:19:02 +0100

    Example macro how to use MC true and match to reconstructed

--- Analysis/Tools/bin/AnalysisMCTrue.cc
+++ Analysis/Tools/bin/AnalysisMCTrue.cc
@@ -26,6 +26,7 @@ int main(int argc, char * argv[])
    // Physics Objects Collections
    analysis.addTree<GenParticle> ("GenParticles","MssmHbb/Events/prunedGenParticles");
    analysis.addTree<GenJet> ("GenJets","MssmHbb/Events/slimmedGenJets");
+   analysis.addTree<Jet> ("Jets","MssmHbb/Events/slimmedJetsReapplyJEC");
 
    
    // Analysis of events
@@ -40,38 +41,123 @@ int main(int argc, char * argv[])
       std::cout << std::endl;
       
       // Generated Particles
+      // The higgs (pdgId == 36) is also in such collection.
+      // If higgs status=22, then hard-process intermediate (mass preserved)
+      // If higgs status=62, then  particle with primordial kT included
+      // The higgs daughters have initially status = 23, but they will undergo several steps 
+      // until status=71, i.e. before hadronisation, which would be the best to use.
+      // Thereofre, one can then try to follow the cascade down to the last b quarks
+      // using delta_R matching. Maybe maybe some delta_pt/pt... but that's not done here.
       auto genParticles = analysis.collection<GenParticle>("GenParticles");
+      GenParticle bquark;
+      GenParticle bbarquark;
       for ( int j = 0 ; j < genParticles->size() ; ++j )
       {
          GenParticle gp = genParticles->at(j);
-         std::cout << "    Generated Particle #" << j << ": ";
-         std::cout << "pT  = "     << gp.pt()      << ", ";
-         std::cout << "eta = "     << gp.eta()     << ", ";
-         std::cout << "phi = "     << gp.phi()     << ", ";
-         std::cout << "pdgId = " << gp.pdgId() << ", ";
-         std::cout << "status = " << gp.status() << ", ";
-         if ( fabs(gp.pdgId()) == 5 ) 
+         if ( gp.pdgId() == 36 && gp.status() > 60 )
          {
-            std::cout << "is higgs daughter = "    << gp.higgsDaughter()    << std::endl;
+            std::cout << "I am the higgs! "           << " : ";
+            std::cout << "pT  = "     << gp.pt()      << ", ";
+            std::cout << "eta = "     << gp.eta()     << ", ";
+            std::cout << "phi = "     << gp.phi()     << ", ";
+            std::cout << "pdgId = "   << gp.pdgId()   << ", ";
+            std::cout << "status = "  << gp.status()  << std::endl;
          }
-         else
+         if ( fabs(gp.pdgId()) == 5 && gp.higgsDaughter() ) 
          {
-            std::cout << std::endl;
+            if ( gp.pdgId() > 0 )  bquark = gp;
+            if ( gp.pdgId() < 0 )  bbarquark = gp;
          }
       }
+
+      std::cout << "   I am a higgs b quark daughter! " << " : ";
+      std::cout << "pT  = "     << bquark.pt()      << ", ";
+      std::cout << "eta = "     << bquark.eta()     << ", ";
+      std::cout << "phi = "     << bquark.phi()     << ", ";
+      std::cout << "pdgId = "   << bquark.pdgId()   << ", ";
+      std::cout << "status = "  << bquark.status()  << std::endl;
+      
+      std::cout << "   I am a higgs bbar quark daughter! " << " : ";
+      std::cout << "pT  = "     << bbarquark.pt()      << ", ";
+      std::cout << "eta = "     << bbarquark.eta()     << ", ";
+      std::cout << "phi = "     << bbarquark.phi()     << ", ";
+      std::cout << "pdgId = "   << bbarquark.pdgId()   << ", ";
+      std::cout << "status = "  << bbarquark.status()  << std::endl;
+      
+// ====================================================================      
       
       // Generated jets
+      // Let's match with the Higgs daughters
       auto genJets = analysis.collection<GenJet>("GenJets");
+      GenJet bgenjet;
+      GenJet bbargenjet;
+      int nb = 0;
+      int nbbar = 0;
       for ( int j = 0 ; j < genJets->size() ; ++j )
       {
          GenJet gjet = genJets->at(j);
-         std::cout << "    Generated Jet #" << j << ": ";
-         std::cout << "pT  = "     << gjet.pt()      << ", ";
-         std::cout << "eta = "     << gjet.eta()     << ", ";
-         std::cout << "phi = "     << gjet.phi()     << ", " << std::endl;
+         if ( gjet.deltaR(bquark) < 0.3 && nb == 0)
+         {
+            bgenjet = gjet ;
+            ++nb;
+         }
+         if ( gjet.deltaR(bbarquark) < 0.3 && nbbar == 0 )
+         {
+            bbargenjet = gjet ;
+            std::cout << "       I am a gen jet matched to a higgs daughter bbar! " << " : ";
+            std::cout << "pT  = "     << gjet.pt()      << ", ";
+            std::cout << "eta = "     << gjet.eta()     << ", ";
+            std::cout << "phi = "     << gjet.phi()     << ", " << std::endl;
+            ++nbbar;
+         }
+         if ( nb > 0 && nbbar > 0 ) break;
       }
       
+      std::cout << "       I am a gen jet matched to a higgs daughter b quark! " << " : ";
+      std::cout << "pT  = "     << bgenjet.pt()      << ", ";
+      std::cout << "eta = "     << bgenjet.eta()     << ", ";
+      std::cout << "phi = "     << bgenjet.phi()     << ", " << std::endl;
       
+      std::cout << "       I am a gen jet matched to a higgs daughter bbar quark! " << " : ";
+      std::cout << "pT  = "     << bbargenjet.pt()      << ", ";
+      std::cout << "eta = "     << bbargenjet.eta()     << ", ";
+      std::cout << "phi = "     << bbargenjet.phi()     << ", " << std::endl;
+      
+// ====================================================================      
+      
+      
+      nb = 0;
+      nbbar = 0;
+      // reconstructed jets
+      // will now try to match the gen jets that are daughter of the higgs to the reconstructed jets
+      auto jets = analysis.collection<Jet>("Jets");
+      Jet bjet;
+      Jet bbarjet;
+      for ( int j = 0 ; j < jets->size() ; ++j )
+      {
+         Jet jet = jets->at(j);
+         if ( bgenjet.deltaR(jet) < 0.3 && nb == 0 )
+         {
+            bjet = jet ;
+            ++nb;
+         }
+         if ( bbargenjet.deltaR(jet) < 0.3 && nbbar == 0 )
+         {
+            bbarjet = jet ;
+            ++nbbar;
+         }
+         if ( nb > 0 && nbbar > 0 ) break;
+         
+      }
+      std::cout << "            I am a reco jet matched to a higgs daughter b quark! " << " : ";
+      std::cout << "pT  = "     << bjet.pt()      << ", ";
+      std::cout << "eta = "     << bjet.eta()     << ", ";
+      std::cout << "phi = "     << bjet.phi()     << ", " << std::endl;
+      
+      std::cout << "            I am a reco jet matched to a higgs daughter bbar quark! " << " : ";
+      std::cout << "pT  = "     << bbarjet.pt()      << ", ";
+      std::cout << "eta = "     << bbarjet.eta()     << ", ";
+      std::cout << "phi = "     << bbarjet.phi()     << ", " << std::endl;
    }
    
 //    
