ef49b7e09c1b9cf82a5cd8885cbc401cedef1c91
Roberval Walsh <roberval.walsh@gmail.com>
2016-11-28 17:55:44 +0100

    Improvement

--- Analysis/Tools/interface/strip.sh
+++ Analysis/Tools/interface/strip.sh
@@ -1,7 +1,7 @@
 
-sed 's/]],/&\n/g' $1  > temp; sed -e 's/{//g' -e 's/}//g' -e 's/"//g' -e 's/,//g' -e 's/]//g' -e 's/\[//g' -e 's/://g'   temp > t ; mv t temp
+sed 's/]],/&\n/g' $1  > temp; sed -e 's/{//g' -e 's/}//g' -e 's/"//g' -e 's/,/ /g' -e 's/]/ /g' -e 's/\[/ /g' -e 's/:/ /g'   temp > t ; mv t temp
 sed -i 's/^[ \t]*//' temp
 sed -i '/^$/d' temp
-cat temp
+#cat temp
 
 
