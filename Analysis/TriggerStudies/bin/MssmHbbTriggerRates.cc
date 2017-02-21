b5d6c9c887dbc5d92bf259277e7a379fd9947962
Roberval Walsh <roberval.walsh@gmail.com>
2017-02-21 09:23:27 +0100

    minor modifications

--- Analysis/TriggerStudies/bin/MssmHbbTriggerRates.cc
+++ Analysis/TriggerStudies/bin/MssmHbbTriggerRates.cc
@@ -26,10 +26,20 @@ TGraphErrors * g_rates_;
 std::string inputList_;
 std::string basename_;
 
+std::map<std::string, double> xsections_;
+
+std::string hltPath_[20];
+
+std::map<std::string, double> CrossSections();
+bool L1DoubleJetC100(const Analysis & );
+
 // =============================================================================================   
 int main(int argc, char * argv[])
 {
+   
    TH1::SetDefaultSumw2();  // proper treatment of errors when scaling histograms
+
+   xsections_ = CrossSections();
 
    h1_["nSelected"] = new TH1F("h_nSelected","",35,27.5,62.5);
    h1_["nTotal"]    = new TH1F("h_nTotal","",35,27.5,62.5);
@@ -46,20 +56,21 @@ int main(int argc, char * argv[])
    
    Analysis analysis(inputList_,"MssmHbbTrigger/Events/EventInfo");
    
-   
    // Physics Objects Collections
    analysis.triggerResults("MssmHbbTrigger/Events/TriggerResults");
-   std::string hltPath[20];
-   hltPath[0] = "HLT_DoubleJetsC100_DoubleBTagCSV_p014_DoublePFJetsC100MaxDeta1p6_v1";
-//   hltPath[0] = "HLT_L1SingleJet20_v1";
+   hltPath_[0] = "HLT_ZeroBias_v";
+   hltPath_[1] = "HLT_L1DoubleJetC100_v";
+//   hltPath[1] = "HLT_DoubleJetsC100_DoubleBTagCSV_p014_DoublePFJetsC100MaxDeta1p6_v";
+//   hltPath[1] = "HLT_L1SingleJet20_v1";
    
    // Trigger objects
    std::vector<std::string> jetTriggerObjects;
    jetTriggerObjects.push_back("hltL1sDoubleJetC100");
-   jetTriggerObjects.push_back("hltDoubleJetsC100");
-   jetTriggerObjects.push_back("hltBTagCaloCSVp014DoubleWithMatching");
-   jetTriggerObjects.push_back("hltDoublePFJetsC100");
-   jetTriggerObjects.push_back("hltDoublePFJetsC100MaxDeta1p6");
+//    jetTriggerObjects.push_back("hltDoubleJetsC100");
+//    jetTriggerObjects.push_back("hltBTagCaloCSVp014DoubleWithMatching");
+//    jetTriggerObjects.push_back("hltDoublePFJetsC100");
+//    jetTriggerObjects.push_back("hltDoublePFJetsC100MaxDeta1p6");
+   
 //   jetTriggerObjects.push_back("hltL1sSingleJet20");
    
    std::string trgobj_path = "MssmHbbTrigger/Events/hltTriggerSummaryAOD/";
@@ -67,17 +78,19 @@ int main(int argc, char * argv[])
       analysis.addTree<TriggerObject>(obj,trgobj_path+obj);
    
    // Get generator filter information
-   FilterResults genFilter = analysis.generatorFilter("MssmHbbTrigger/Metadata/GeneratorFilter");  
+//   FilterResults genFilter = analysis.generatorFilter("MssmHbbTrigger/Metadata/GeneratorFilter");  
    
    // Get cross section
    analysis.crossSections("MssmHbbTrigger/Metadata/CrossSections");
    
-   double crossSection = analysis.crossSection();
+   double crossSection = xsections_[basename_];
+   
+   std::cout << crossSection << std::endl;
    
    // Analysis of events
    std::cout << "This analysis has " << analysis.size() << " events" << std::endl;
    
-   float genFilterEfficiency = genFilter.efficiency;
+//   float genFilterEfficiency = genFilter.efficiency;
    for ( int i = 0 ; i < analysis.size() ; ++i )
 //   for ( int i = 0 ; i < 300000 ; ++i )
    {
@@ -88,93 +101,94 @@ int main(int argc, char * argv[])
          std::cout << i << " events analysed" << std::endl;
       }
       
+      // Trigger results: fired and prescales
+      // hltPath0 - reference trigger
+      if ( ! analysis.triggerResult(hltPath_[0]) ) continue;
+      
       h1_["nTotal"] -> Fill(analysis.nTruePileup());
       
-      // Trigger results: fired and prescales
-      // hltPath1
-      int trg_fired = analysis.triggerResult(hltPath[0]);
+      int trg_fired = analysis.triggerResult(hltPath_[1]);
       
       if ( ! trg_fired ) continue;
       
       // FOR L1 TRIGGER
-//       auto l1jet20 = analysis.collection<TriggerObject>("hltL1sSingleJet20");
-//       if ( l1jet20->size() < 2 ) continue;
-//       
-//       std::vector<TriggerObject> l1dijetc100;
-//       for ( int j = 0 ; j < l1jet20->size() ; ++j )
-//       {
-//          TriggerObject jet = l1jet20->at(j);
-//          if ( jet.pt() < 100. || fabs(jet.eta()) > 2.3 ) continue;
-//          l1dijetc100.push_back(jet);
-//       }
-//       
-//       if ( l1dijetc100.size() < 2 ) continue;
-//       
-//       bool deta = false;
-//       for ( int j1 = 0 ; j1 < (int)l1dijetc100.size()-1 ; ++j1 )
-//       {
-//          for ( int j2 = j1+1 ; j2 < (int)l1dijetc100.size() ; ++j2 )
-//          {
-//             float eta1 = l1dijetc100.at(j1).eta();
-//             float eta2 = l1dijetc100.at(j2).eta();
-//             if ( fabs(eta1-eta2) <= 1.6 ) deta = true;
-//          }
-//       }
-//       
-//       if ( ! deta ) continue;
+      auto l1dijet100 = analysis.collection<TriggerObject>("hltL1sDoubleJetC100");
+      if ( l1dijet100->size() < 2 ) continue;
+      
+      std::vector<TriggerObject> l1dijetc100v2;
+      for ( int j = 0 ; j < l1dijet100->size() ; ++j )
+      {
+         TriggerObject jet = l1dijet100->at(j);
+         if ( jet.pt() < 100. || fabs(jet.eta()) > 2.3 ) continue;
+         l1dijetc100v2.push_back(jet);
+      }
+      
+      if ( l1dijetc100v2.size() < 2 ) continue;
+      
+      bool deta = false;
+      for ( int j1 = 0 ; j1 < (int)l1dijetc100v2.size()-1 ; ++j1 )
+      {
+         for ( int j2 = j1+1 ; j2 < (int)l1dijetc100v2.size() ; ++j2 )
+         {
+            float eta1 = l1dijetc100v2.at(j1).eta();
+            float eta2 = l1dijetc100v2.at(j2).eta();
+            if ( fabs(eta1-eta2) <= 1.6 ) deta = true;
+         }
+      }
+      
+      if ( ! deta ) continue;
       
       
       // All hadronic path
       
-      // L1 
-      auto l1dijetc100 = analysis.collection<TriggerObject>("hltL1sDoubleJetC100");
-      std::vector<TriggerObject> l1dijet100eta2p3;
-      for ( int j = 0 ; j < l1dijetc100->size() ; ++j )
-      {
-         TriggerObject jet = l1dijetc100->at(j);
-         if ( fabs(jet.eta()) > 2.3 ) continue;
-         l1dijet100eta2p3.push_back(jet);
-      }
-      
-      if ( l1dijet100eta2p3.size() < 2 ) continue;
-      
-      bool deta = false;
-      for ( int j1 = 0 ; j1 < (int)l1dijet100eta2p3.size()-1 ; ++j1 )
-      {
-         for ( int j2 = j1+1 ; j2 < (int)l1dijet100eta2p3.size() ; ++j2 )
-         {
-            float eta1 = l1dijet100eta2p3.at(j1).eta();
-            float eta2 = l1dijet100eta2p3.at(j2).eta();
-            if ( fabs(eta1-eta2) <= 1.6 ) deta = true;
-         }
-      }
-      if ( ! deta ) continue;
-      
-      // online b-tagging trigger objects
-      // at most 6 jets with pT > 80 GeV
-      auto hlt8bJets30 = analysis.collection<TriggerObject>("hltBTagCaloCSVp014DoubleWithMatching");
-      std::vector<TriggerObject> hlt6bJets80;
-      for ( int j = 0 ; j < hlt8bJets30->size() ; ++j )
-      {
-         if ( j == 6 ) break; // at most 6 jets
-         TriggerObject jet = hlt8bJets30->at(j);
-         if ( jet.pt() < 80. ) continue;
-         hlt6bJets80.push_back(jet);
-      }
-      
-      if ( hlt6bJets80.size() < 2 ) continue;
-      
-      auto hltdipf100 = analysis.collection<TriggerObject>("hltDoublePFJetsC100");
-      std::vector<TriggerObject> hltdipf100eta2p3;
-      for ( int j = 0 ; j < hltdipf100->size() ; ++j )
-      {
-         TriggerObject jet = hltdipf100->at(j);
-         if ( fabs(jet.eta()) > 2.3 ) continue;
-         hltdipf100eta2p3.push_back(jet);
-      }
-      
-      if ( hltdipf100eta2p3.size() < 2 ) continue;
-      
+//       // L1 
+//       auto l1dijetc100 = analysis.collection<TriggerObject>("hltL1sDoubleJetC100");
+//       std::vector<TriggerObject> l1dijet100eta2p3;
+//       for ( int j = 0 ; j < l1dijetc100->size() ; ++j )
+//       {
+//          TriggerObject jet = l1dijetc100->at(j);
+//          if ( fabs(jet.eta()) > 2.3 ) continue;
+//          l1dijet100eta2p3.push_back(jet);
+//       }
+//       
+//       if ( l1dijet100eta2p3.size() < 2 ) continue;
+//       
+//       bool deta = false;
+//       for ( int j1 = 0 ; j1 < (int)l1dijet100eta2p3.size()-1 ; ++j1 )
+//       {
+//          for ( int j2 = j1+1 ; j2 < (int)l1dijet100eta2p3.size() ; ++j2 )
+//          {
+//             float eta1 = l1dijet100eta2p3.at(j1).eta();
+//             float eta2 = l1dijet100eta2p3.at(j2).eta();
+//             if ( fabs(eta1-eta2) <= 1.6 ) deta = true;
+//          }
+//       }
+//       if ( ! deta ) continue;
+//       
+//       // online b-tagging trigger objects
+//       // at most 6 jets with pT > 80 GeV
+//       auto hlt8bJets30 = analysis.collection<TriggerObject>("hltBTagCaloCSVp014DoubleWithMatching");
+//       std::vector<TriggerObject> hlt6bJets80;
+//       for ( int j = 0 ; j < hlt8bJets30->size() ; ++j )
+//       {
+//          if ( j == 6 ) break; // at most 6 jets
+//          TriggerObject jet = hlt8bJets30->at(j);
+//          if ( jet.pt() < 80. ) continue;
+//          hlt6bJets80.push_back(jet);
+//       }
+//       
+//       if ( hlt6bJets80.size() < 2 ) continue;
+//       
+//       auto hltdipf100 = analysis.collection<TriggerObject>("hltDoublePFJetsC100");
+//       std::vector<TriggerObject> hltdipf100eta2p3;
+//       for ( int j = 0 ; j < hltdipf100->size() ; ++j )
+//       {
+//          TriggerObject jet = hltdipf100->at(j);
+//          if ( fabs(jet.eta()) > 2.3 ) continue;
+//          hltdipf100eta2p3.push_back(jet);
+//       }
+//       
+//       if ( hltdipf100eta2p3.size() < 2 ) continue;
       
 //       // muon selection at L1
 //       auto l1Mu3s = analysis.collection<TriggerObject>("hltL1sSingleMu3");
@@ -234,7 +248,7 @@ int main(int argc, char * argv[])
    g_rates_ = new TGraphErrors(35,pu,rate,puErr,rateErr);
    g_rates_ -> SetName("rates");
    
-   TFile * f_out = new TFile(Form("hltrates_mod_%s.root",basename_.c_str()),"RECREATE");
+   TFile * f_out = new TFile(Form("xxxl1rates_mod_%s.root",basename_.c_str()),"RECREATE");
    for ( auto & h : h1_ )
    {
       std::cout << h.first << std::endl;
@@ -254,6 +268,27 @@ int main(int argc, char * argv[])
    
 //    
 }
+
+
+bool L1DoubleJetC100(const Analysis & analysis)
+{
+   return true;
+}
+
+std::map<std::string, double> CrossSections()
+{
+   std::map<std::string, double> xsections;
+   xsections["QCD_Pt_15to30"]   = 1.83741e+09;
+   xsections["QCD_Pt_30to50"]   = 1.40932e+08;
+   xsections["QCD_Pt_50to80"]   = 1.92043e+07;
+   xsections["QCD_Pt_80to120"]  = 2.76253e+06;
+   xsections["QCD_Pt_120to170"] = 471100;
+   xsections["QCD_Pt_170to300"] = 117276;
+   xsections["QCD_Pt_300to470"] = 7823.28;
+   xsections["QCD_Pt_470to600"] = 648.174;
+   return xsections;
+}
+
 
 // TRIGGER PATHS
 //                                   'HLT_L1SingleJet20_v1',
